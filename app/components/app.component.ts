4e07fb57219b189b318dfe3e94b96f35fb8fa5ca
jhyde <jeff@jeffhyde.com>
2016-01-07 12:43:19 -0700

    Added routing

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,16 +1,26 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+
+import {Login} from './user-management/login.component';
+import {Register} from './user-management/register.component'
+// import {Home} from './home/home.component'
+
 import { MyService } from '../services/sampleService';
-import {Login} from './user-management/login.component'
 
 @Component({
   selector: 'app',
   bindings: [MyService],
   templateUrl: 'app/components/app.component.html',
-  styles: [`p {color:red}`],
-  directives: [Login],
+  styles: [`pre {color:red}`],
+  directives: [ROUTER_DIRECTIVES, Login],
   providers: [MyService]
 })
 
+@RouteConfig([
+  // { path: '/home',  name: 'Home', component: Home, useAsDefault: true},
+  { path: '/user-management/register',  name: 'Register', component: Register},
+  { path: '/user-management/login', name: 'Login', component: Login}
+])
 export class AppComponent {
   location: Location;
   appStatus: string;
