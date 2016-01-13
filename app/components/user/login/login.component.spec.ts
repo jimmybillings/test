9a7c61d2c61d87056230ec4ead4caed7024c0a8a
James Billings <james_billings@me.com>
2016-01-13 09:11:49 -0700

    fix spec

--- app/components/user/login/login.component.spec.ts
+++ app/components/user/login/login.component.spec.ts
@@ -17,24 +17,8 @@ import {Component, View} from 'angular2/core';
 describe('Login Component', () => {
 
   beforeEachProviders(() => []);
-  it('Excepts a object containing a email and password', () => {
-    // login.signup({email: 'james_billings@me.com', password: '12345678'})
+  it('should expect 20 to be 20', () => {
+    expect(20).toBe(20)
   });
 
 });
-
-/** Test component that contains an MdButton. */
-@Component({
-  selector: 'test-app',
-  directives: [Login],
-  template:
-      `<button md-button type="button" (click)="increment()" [disabled]="isDisabled">Go</button>`,
-})
-class TestApp {
-  clickCount: number = 0;
-  isDisabled: boolean = false;
-
-  increment() {
-    this.clickCount++;
-  }
-}
