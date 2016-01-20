2b4a9d1f1e873afc542238075b9d74743a54306d
jhyde <jeff@jeffhyde.com>
2016-01-19 20:53:24 -0700

    Add class change to header when scrolling beyond a certain point.

--- app/components/application/header/header.component.ts
+++ app/components/application/header/header.component.ts
@@ -1,4 +1,5 @@
 import {Component} from 'angular2/core';
+import {NgClass} from 'angular2/common';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import {Logout} from '../../user-management/logout/logout.component'
 import {CurrentUser} from '../../../common/models/current-user.model'
@@ -6,30 +7,36 @@ import {CurrentUser} from '../../../common/models/current-user.model'
 @Component({
   selector: 'app-header',
   templateUrl: '/app/components/application/header/header.html',
-  directives: [ROUTER_DIRECTIVES, Logout],
-  providers:[CurrentUser]
+  directives: [ROUTER_DIRECTIVES, Logout, NgClass],
+  providers:[CurrentUser],
 })
 
-export class Header {    
+
+
+
+export class Header {
   public currentUser: CurrentUser;
+  isScrolled: boolean = false;
   
   constructor(currentUser: CurrentUser) {
+    window.addEventListener('scroll', this.showScrollingHeader, false);
     this.currentUser = currentUser;
-    window.addEventListener('scroll', this._showScrollingHeader, false);
+  }
+  public toggle(newState) {
+    this.isScrolled = newState;
   }
 
-  private _showScrollingHeader(e) {
-    let isScrolled: boolean = false;
-    
+  public showScrollingHeader() {
     if (window.pageYOffset > 80)
     {
-      isScrolled = true;
-      console.log(isScrolled);
+      this.isScrolled = true;
+      console.log(this.isScrolled);
       console.log(window.pageYOffset);
     }
     else
-      isScrolled = false;
-      console.log(isScrolled);
+    {
+      this.isScrolled = false;
+      console.log(this.isScrolled);
+    }
   }
-  
 }
