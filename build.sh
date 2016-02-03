d911d0545e1ad90c5fbed713297c26d5a17f1479
James Billings <james_billings@me.com>
2016-02-03 15:11:40 -0700

    update build

--- build.sh
+++ build.sh
@@ -37,7 +37,6 @@ print-build-environment.sh
 buildVersion=$(update-maven-version-for-build.sh)                                        || exit 1 
 
 # Build
-rm -rf node_modules
 npm install
 npm run build.prod                                                                       || exit 1
 
