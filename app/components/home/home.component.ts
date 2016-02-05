dec858a84eaf2773b5c67acd7bbac29471dff9d9
jhyde <jeff@jeffhyde.com>
2016-02-04 18:39:14 -0700

    Created new search box component

--- app/components/home/home.component.ts
+++ app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import {NgIf} from 'angular2/common';
 import {CurrentUser} from '../../common/models/current-user.model';
+import {SearchBox} from './search-box/search-box.component';
 
 @Component({
   selector: 'home',
   templateUrl: 'components/home/home.html',
-  directives: [ROUTER_DIRECTIVES, NgIf]
+  directives: [ROUTER_DIRECTIVES, NgIf, SearchBox]
 })
 
 export class Home {
@@ -14,5 +15,9 @@ export class Home {
   
   constructor(currentUser: CurrentUser) {
     this.currentUser = currentUser;
+  }
+  
+  searchAssets(text: string): void {
+    console.log(text);
   }
 }
