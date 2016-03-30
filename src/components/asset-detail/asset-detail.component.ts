800fdaff2bcf819f4773225393d1ec13c4c74f89
jhyde <jeff@jeffhyde.com>
2016-03-30 07:56:52 -0600

    CRUX-125 added some mock data

--- src/components/asset-detail/asset-detail.component.ts
+++ src/components/asset-detail/asset-detail.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, ChangeDetectionStrategy} from 'angular2/core';
-import {COMMON_DIRECTIVES} from 'angular2/common';
+import {COMMON_DIRECTIVES, NgIf, NgFor} from 'angular2/common';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
 
 /**
  * Directive that renders details of a single asset
@@ -8,7 +9,7 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
 @Component({
   selector: 'asset-detail',
   templateUrl: 'components/asset-detail/asset-detail.html',
-  directives: [COMMON_DIRECTIVES, ROUTER_DIRECTIVES],
+  directives: [COMMON_DIRECTIVES, ROUTER_DIRECTIVES, MATERIAL_DIRECTIVES, NgIf, NgFor],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
