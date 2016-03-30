e2ecf952755b717eea47e251e97c0b5fe0e9e71a
jhyde <jeff@jeffhyde.com>
2016-03-30 09:17:28 -0600

    CRUX-125 fixed broken test

--- src/containers/search/search.component.spec.ts
+++ src/containers/search/search.component.spec.ts
@@ -20,6 +20,7 @@ import { ApiConfig } from '../../common/config/api.config';
 import {CurrentUser} from '../../common/models/current-user.model';
 import {UiConfig, config} from '../../common/config/ui.config';
 import {AssetData} from './services/asset.data.service';
+import { AssetService } from '../../containers/asset/services/asset.service';
 import {Observable} from 'rxjs/Rx';
 import { provideStore } from '@ngrx/store';
 
@@ -55,6 +56,7 @@ export function main() {
       provideStore({config: config}),
       CurrentUser,
       ApiConfig,
+      AssetService,
       provide(AssetData, {useClass: MockAssetData}),
       provide(UiConfig, {useClass: MockUiConfig}),
     ]);
