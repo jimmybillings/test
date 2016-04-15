cecdb258f7929c3a6374f7b27ec1ae0a37be3eb1
rossedfort <rossedfort@gmail.com>
2016-04-15 15:53:02 -0600

    CRUX-196 fix broken test

--- src/containers/admin/admin.component.ts
+++ src/containers/admin/admin.component.ts
@@ -1,6 +1,5 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
-import {CurrentUser} from '../../common/models/current-user.model';
 import {Dashboard} from './dashboard/dashboard.component';
 import {Account} from './account/account.component';
 
@@ -15,10 +14,4 @@ import {Account} from './account/account.component';
   { path: '/accounts', component: Account, name: 'Account' }
 ])
 
-export class Admin {
-  public currentUser: CurrentUser;
-  
-  constructor(currentUser: CurrentUser) {
-    this.currentUser = currentUser;
-  }
-}
+export class Admin {}
