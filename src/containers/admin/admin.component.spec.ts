76cade35951badbad687ede3a4ec8ba80389fab4
rossedfort <rossedfort@gmail.com>
2016-04-18 09:02:44 -0600

    CRUX-196 move admin nav bar to top level admin component

--- src/containers/admin/admin.component.spec.ts
+++ src/containers/admin/admin.component.spec.ts
@@ -7,11 +7,13 @@ it,
 beforeEachProviders
 } from 'angular2/testing';
 
+import {CurrentUser, currentUser} from '../../common/models/current-user.model';
 import {provide} from 'angular2/core';
 import {Location, Router, RouteRegistry, ROUTER_PRIMARY_COMPONENT} from 'angular2/router';
 import {SpyLocation} from 'angular2/src/mock/location_mock';
 import {RootRouter} from 'angular2/src/router/router';
 import {Admin} from './admin.component';
+import {provideStore} from '@ngrx/store';
 
 export function main() {
   describe('Admin Component', () => {
@@ -20,6 +22,8 @@ export function main() {
       provide(Location, { useClass: SpyLocation }),
       provide(ROUTER_PRIMARY_COMPONENT, { useValue: Admin }),
       provide(Router, { useClass: RootRouter }),
+      provideStore({currentUser: currentUser}),
+      CurrentUser
     ]);
     
     it('Should have an admin instance',
--- src/containers/admin/admin.component.ts
+++ src/containers/admin/admin.component.ts
@@ -1,11 +1,12 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {CurrentUser} from '../../common/models/current-user.model';
 import {Dashboard} from './dashboard/dashboard.component';
 import {Account} from './account/account.component';
 
 @Component({
   selector: 'admin',
-  template: '<router-outlet></router-outlet>',
+  templateUrl: 'containers/admin/admin.html',
   directives: ROUTER_DIRECTIVES
 })
 
@@ -14,4 +15,10 @@ import {Account} from './account/account.component';
   { path: '/accounts', component: Account, name: 'Account' }
 ])
 
-export class Admin {}
+export class Admin {
+  public currentUser: CurrentUser;
+  
+  constructor(currentUser: CurrentUser) {
+    this.currentUser = currentUser;
+  }
+}
