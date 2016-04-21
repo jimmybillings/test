dc21d1e2d6219cd4cf688062f1f038c9df19c017
rossedfort <rossedfort@gmail.com>
2016-04-21 11:47:22 -0600

    CRUX-197 Implement md-data-table
    this required an update of the ng2-material package

--- src/components/wz-list/wz.list.component.ts
+++ src/components/wz-list/wz.list.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, ChangeDetectionStrategy } from 'angular2/core';
+import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
 import {NgFor} from 'angular2/common';
 
 @Component({
   selector: 'wz-list',
   templateUrl: 'components/wz-list/wz.list.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  directives: [NgFor]
+  directives: [NgFor, MATERIAL_DIRECTIVES]
 })
 
 export class WzList {
