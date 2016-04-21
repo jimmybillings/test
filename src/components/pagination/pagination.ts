0e1338d54cafad5b1d5965fae650a7d113260f1b
rossedfort <rossedfort@gmail.com>
2016-04-21 11:54:49 -0600

    CRUX-197 Implement basic buttons in pagination component

--- src/components/pagination/pagination.ts
+++ src/components/pagination/pagination.ts
@@ -1,9 +1,11 @@
 import { Component, Input, ChangeDetectionStrategy } from 'angular2/core';
+import { MATERIAL_DIRECTIVES } from 'ng2-material/all';
 
 @Component({
   selector: 'pagination',
   templateUrl: 'components/pagination/pagination.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  directives: [MATERIAL_DIRECTIVES]
 })
 
 export class Pagination {
