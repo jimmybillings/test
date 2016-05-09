a6a28946b9570901a5f3b4fed277372d8b84ace6
rossedfort <rossedfort@gmail.com>
2016-05-09 11:27:23 -0600

    CRUX-224 - fix test

--- src/containers/admin/index/index.component.spec.ts
+++ src/containers/admin/index/index.component.spec.ts
@@ -94,7 +94,7 @@ export function main() {
       component.pageSize = {'value': '10'};
       spyOn(component.router, 'navigate');
       component.navigateToPageUrl(2);
-      expect(component.router.navigate).toHaveBeenCalledWith([ '/Admin/Account', Object({ i: 2, n: 10, s: 'createdOn', d: true}) ]);
+      expect(component.router.navigate).toHaveBeenCalledWith([ '/Admin/Account', Object({ i: 2, n: 10, s: 'createdOn', d: true, fields: '', values: ''}) ]);
     }));
     
     it('Should have a navigateToSortUrl function that navigates to a URL with correct params', inject([Index], (component) => {
@@ -102,7 +102,7 @@ export function main() {
       component.pageSize = {'value': '10'};
       spyOn(component.router, 'navigate');
       component.navigateToSortUrl({attr: 'emailAddress', toggle: true});
-      expect(component.router.navigate).toHaveBeenCalledWith([ '/Admin/Account', Object({ i: 1, n: 10, s: 'emailAddress', d: true}) ]);
+      expect(component.router.navigate).toHaveBeenCalledWith([ '/Admin/Account', Object({ i: 1, n: 10, s: 'emailAddress', d: true, fields: '', values: ''}) ]);
     }));
     
     it('Should have a navigateToFilterUrl function that navigates to a URL with correct params', inject([Index], (component) => {
