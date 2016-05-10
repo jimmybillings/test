6b45e77fd7af805f8523a186a8ea804b117dbde1
Carolyne Billings <carolynebillings@Carolynes-MacBook-Air.local>
2016-05-09 20:30:46 -0600

    INTERNAL - migrate jade to HTML

--- tools/tasks/build.jade.dev.ts
+++ tools/tasks/build.jade.dev.ts
@@ -2,8 +2,7 @@ import {APP_SRC, APP_DEST} from '../config';
 
 export = function buildJadeDev(gulp, plugins, option) {
   return function () {
-    return gulp.src(APP_SRC+'/**/*.jade')
-    .pipe(plugins.jade())
+    return gulp.src(APP_SRC+'/**/*.html')
     .pipe(gulp.dest(APP_DEST));
   };
 }
