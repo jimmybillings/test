17f86ab5e42fa363ac8a644b14a3f89e87a959ec
jhyde <jeff@jeffhyde.com>
2016-05-11 15:31:40 -0600

    CRUX-136 added translation support for meta data field names.

--- src/components/asset-detail/asset-detail.component.ts
+++ src/components/asset-detail/asset-detail.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, ChangeDetectionStrategy} from 'angular2/core';
+import {TranslatePipe} from 'ng2-translate/ng2-translate';
 import {COMMON_DIRECTIVES, NgIf, NgFor } from 'angular2/common';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
@@ -11,6 +12,7 @@ import {Player} from '../../components/player/player.component';
   selector: 'asset-detail',
   templateUrl: 'components/asset-detail/asset-detail.html',
   directives: [COMMON_DIRECTIVES, ROUTER_DIRECTIVES, MATERIAL_DIRECTIVES, NgIf, NgFor, Player],
+  pipes: [TranslatePipe],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
@@ -24,5 +26,9 @@ export class AssetDetail {
   public getMetaField(field) {
     let meta = this.assetDetail.clipData.filter(item => item.name === field)[0];  
     if (meta) return meta.value;
+  }
+  
+  public translationReady(field) {
+    return 'assetmetadata.'+field.replace(/\./g, '_');
   }
 }
