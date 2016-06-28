0e2bbe7938d067201c27a63031c76822d6f9f5a2
rossedfort <rossedfort@gmail.com>
2016-06-28 10:50:10 -0600

    INTERNAL add toast and adjust styling for secret config edit

--- src/client/app/+admin/+secret-config/secret-config.component.ts
+++ src/client/app/+admin/+secret-config/secret-config.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import {UiConfig} from '../../shared/services/ui.config';
-import {FormBuilder, Validators, ControlGroup, Control} from '@angular/common';
-import {ConfigService} from '../services/config.service';
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { UiConfig } from '../../shared/services/ui.config';
+import { FormBuilder, Validators, ControlGroup, Control } from '@angular/common';
+import { ConfigService } from '../services/config.service';
 import { RouteSegment } from '@angular/router';
+import { ToastService } from '../../shared/components/toast/toast.service';
 
 @Component({
   moduleId: module.id,
   selector: 'secret-config',
   templateUrl: 'secret-config.html',
-  providers: [ConfigService],
+  providers: [ConfigService, ToastService],
   styles: [`.secret-config {
               display: block;
               padding-top:40px;
@@ -26,16 +27,19 @@ import { RouteSegment } from '@angular/router';
 
 export class SecretConfigComponent implements OnInit {
   public config: any;
+  public site: string;
+  @ViewChild('target', { read: ViewContainerRef }) target: any;
   private configForm: ControlGroup;
 
   constructor(public uiConfig: UiConfig,
+    public toastService: ToastService,
     public fb: FormBuilder,
     public configService: ConfigService,
     public routeSegment: RouteSegment) { }
 
   ngOnInit(): void {
-    let site = this.routeSegment.getParam('site');
-    this.configService.getUiConfig(site).subscribe((data: any) => {
+    this.site = this.routeSegment.getParam('site');
+    this.configService.getUiConfig(this.site).subscribe((data: any) => {
       this.config = data;
       this.setForm();
     });
@@ -48,8 +52,11 @@ export class SecretConfigComponent implements OnInit {
   public onSubmit(form: any): void {
     this.configService.update(JSON.parse(form.config))
       .subscribe((res) => {
+        this.toastService.createToast(`${this.site} config updated`, 'success', 5000, this.target);
         this.uiConfig.set(res.json());
         (<Control>this.configForm.controls['config']).updateValue(JSON.stringify(res.json(), undefined, 4));
+      }, (err) => {
+        this.toastService.createToast('error!' + err._body, 'success', 5000, this.target);
       });
   }
 }
