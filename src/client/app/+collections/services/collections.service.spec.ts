abafd651afd58dc6861fd1536e83ab88a5c2ae75
jhyde <jeff@jeffhyde.com>
2016-06-30 17:22:04 -0600

    CRUX-396 fixed failing collection test

--- src/client/app/+collections/services/collections.service.spec.ts
+++ src/client/app/+collections/services/collections.service.spec.ts
@@ -101,8 +101,8 @@ export function main() {
         service.setFocusedCollection(158).subscribe(response => {
           expect(connection.request.url).toBe('api/identites/v1/collection/focused/158');
           expect(connection.request.method).toBe(RequestMethod.Put);
-          expect(response._body).toEqual(mockCollection());
-          expect(response._body.id).toEqual(158);
+          expect(response).toEqual(mockCollection());
+          expect(response.id).toEqual(158);
         });
         connection.mockRespond(new Response(
           new ResponseOptions({
