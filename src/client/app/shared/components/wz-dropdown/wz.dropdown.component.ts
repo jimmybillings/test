fcf2723de75a6626b9bdce38c2b8e36b5e9d6c3c
jhyde <jeff@jeffhyde.com>
2016-07-06 15:02:19 -0600

    CRUX-513 changed drop down position when screen size less than 600px

--- src/client/app/shared/components/wz-dropdown/wz.dropdown.component.ts
+++ src/client/app/shared/components/wz-dropdown/wz.dropdown.component.ts
@@ -63,11 +63,15 @@ export class WzDropdownComponent {
   }
 
   private positionElement(event: any) {
-    this.config.positionStrategy = this.overlay.position()
-      .global()
-      .fixed()
-      .right(window.outerWidth - event.clientX + 'px')
-      .top(event.clientY + 'px');
+    let offset: number = 25;
+    let layoutBreakpointXs: boolean = event.view.screen.width < 600;
+    if (layoutBreakpointXs) {
+      this.config.positionStrategy =
+        this.overlay.position().global().fixed().right('0').top('0');
+    } else {
+      this.config.positionStrategy =
+        this.overlay.position().global().fixed().right(window.outerWidth - event.clientX - offset + 'px').top(event.clientY - offset + 'px');
+    }
   }
 
   private closeListener() {
