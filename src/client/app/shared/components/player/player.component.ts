597782cae553aea1ad58cc5a3c635001ca72a178
James Billings <james_billings@me.com>
2016-07-06 10:08:56 -0600

    INTERNAL - clean up player code

--- src/client/app/shared/components/player/player.component.ts
+++ src/client/app/shared/components/player/player.component.ts
@@ -20,14 +20,7 @@ export class PlayerComponent implements OnChanges {
   public player: { load: any };
 
   ngOnChanges(changes:any) {
-    console.log(changes);
     this.player = new PlayerApi(document.querySelector('iframe#player'), { environment: PlayerEnvironment.PRODUCTION });
     this.player.load(changes.clip.currentValue, 'tem-r5tHustu');
   }
-
-  // ngOnChanges(changes): void {
-  //   console.log(changes.clip.currentValue);
-  //   console.log(changes.clip);
-  //   if (changes.clip && changes.clip.currentValue) this.player.load(changes.clip.currentValue, 'tem-r5tHustu');
-  // }
 }
