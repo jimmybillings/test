7a356ffae893077a49c376c4578924cb7b7f629f
rossedfort <rossedfort@gmail.com>
2016-07-12 14:58:08 -0600

    CRUX-467 #time 30m #resolve #comment update admin new and edit

--- src/client/app/+admin/admin.routes.ts
+++ src/client/app/+admin/admin.routes.ts
@@ -1,8 +1,6 @@
 import {RouterConfig} from '@angular/router';
 import {DashboardComponent} from './+dashboard/dashboard.component';
 import {IndexComponent} from './+index/index.component';
-import {NewComponent} from './+new/new.component';
-import {EditComponent} from './+edit/edit.component';
 import {ConfigComponent} from './+config/config.component';
 import {UiConfigComponent} from './+ui-config/ui-config.component';
 import {SiteConfigComponent} from './+site-config/site-config.component';
@@ -13,8 +11,6 @@ export const ADMIN_ROUTES: RouterConfig = [
   { path: 'config', component: ConfigComponent},
   { path: 'ui-config/:site', component: UiConfigComponent },
   { path: 'site-config/:site', component: SiteConfigComponent },
-  { path: 'resource/:resource/new', component: NewComponent },
-  { path: 'resource/:resource/:id', component: EditComponent },
   { path: 'resource/:resource', component: IndexComponent },
   { path: 'secret-config/:site', component: SecretConfigComponent }
 ];
