46e9ec789d05ab5541288e926ffbef15faa5c966
jhyde <jeff@jeffhyde.com>
2016-07-12 15:00:30 -0600

    CRUX-544 - reverting changes

--- src/client/app/shared/components/bin-tray/bin-tray.component.ts
+++ src/client/app/shared/components/bin-tray/bin-tray.component.ts
@@ -56,15 +56,13 @@ export class BinTrayComponent {
         });
       }
       this.collectionsService.storeCollections(payload);
+
       // get focused collection assets and thumbnails
       if (payload.totalCount > 0) {
         this.collectionsService.getFocusedCollection().first().subscribe(collection => {
           if (collection.assets) {
-            this.collectionsService.getCollectionItems(collection.id, collection.assets.length).subscribe(search => {
-              this.collectionsService.getCollectionItems(collection.id, 1, collection.assets.length - 1).subscribe(thumbnail => {
-                this.collectionsService.updateFocusedCollectionAssets(collection, search, thumbnail);
-                // this.collectionsService.updateFocusedCollectionAssets(collection, search);
-              });
+            this.collectionsService.getCollectionItems(collection.id, 300).first().subscribe(search => {
+              this.collectionsService.updateFocusedCollectionAssets(collection, search);
             });
           } else {
             this.collectionsService.updateFocusedCollection(collection);
@@ -74,5 +72,4 @@ export class BinTrayComponent {
     });
   }
   // error => this.error.handle(error);
-
 }
