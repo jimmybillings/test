cebb8fe738f88b2efcbb15032c7c60b64eee6431
rossedfort <ross.edfort@wazeedigital.com>
2016-07-19 11:31:46 -0600

    CRUX-506 complete home component specs CRUX-506 #time 30m #resolve

--- src/client/app/+home/home.component.spec.ts
+++ src/client/app/+home/home.component.spec.ts
@@ -25,7 +25,7 @@ export function main() {
 
     class MockUiConfig {
       get(comp: any) {
-        return Observable.of({ 'components': { 'component': 'true' }, 'config': { 'config': 'true' } });
+        return Observable.of({'config': {'pageSize': {'value': 100}}});
       }
     }
     class MockRouter {}
@@ -51,6 +51,15 @@ export function main() {
       SearchContext
     ]);
 
+    it('Should have router, apiConfig, currentUser, searchContext and uiConfig defined',
+      inject([HomeComponent], (component: HomeComponent) => {
+        expect(component.currentUser).toBeDefined();
+        expect(component.router).toBeDefined();
+        expect(component.apiConfig).toBeDefined();
+        expect(component.currentUser).toBeDefined();
+        expect(component.searchContext).toBeDefined();
+      }));
+
     it('Create instance of home and assign the CurrentUser to an instance variable inside of home',
       inject([TestComponentBuilder], (tcb: any) => {
         tcb.createAsync(HomeComponent).then((fixture: any) => {
@@ -64,8 +73,16 @@ export function main() {
       spyOn(component.uiConfig, 'get').and.callThrough();
       component.ngOnInit();
       expect(component.uiConfig.get).toHaveBeenCalledWith('home');
-      expect(component.config).toEqual({ 'config': 'true' });
+      expect(component.config).toEqual({'pageSize': { 'value': 100 }});
     }));
+
+    it('Should have a newSearchContext() method that creates a new search context',
+      inject([HomeComponent], (component: HomeComponent) => {
+        component.ngOnInit();
+        spyOn(component.searchContext, 'new');
+        component.newSearchContext('cat');
+        expect(component.searchContext.new).toHaveBeenCalledWith({ q: 'cat', i: 1, n: 100 });
+      }));
 
   });
 }
