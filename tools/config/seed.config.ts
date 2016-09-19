f02c63bca63382da434be614b5a2744ff657c4f1
rossedfort <ross.edfort@wazeedigital.com>
2016-09-19 10:45:23 -0600

    INTERNAL - fix app by removing @ngrx/store path from system config

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -367,7 +367,7 @@ export class SeedConfig {
       '@angular2-material/toolbar': 'node_modules/@angular2-material/toolbar/toolbar.umd.js',
       '@angular2-material/tooltip': 'node_modules/@angular2-material/tooltip/tooltip.umd.js',
       'rxjs/*': 'node_modules/rxjs/*',
-      '@ngrx/store': 'node_modules/@ngrx/store/index.js',
+      // '@ngrx/store': 'node_modules/@ngrx/store/index.js',
       'app/*': '/app/*',
       // For test config
       'dist/dev/*': '/base/dist/dev/*',
