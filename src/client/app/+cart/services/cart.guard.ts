77fc91745b1f2597ee4b2923daa9141c3c19a421
rossedfort <ross.edfort@wazeedigital.com>
2016-09-27 13:15:14 -0600

    CRUX-763 - implement cart guard #time 30m #resolve

---
+++ src/client/app/+cart/services/cart.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { UserPermission } from '../../shared/services/permission.service';
+import { CurrentUser } from '../../shared/services/current-user.model';
+
+@Injectable()
+export class CartGuard implements CanActivate {
+  constructor(
+    private currentUser: CurrentUser,
+    private permission: UserPermission,
+    private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return (this.canView()) ? true : this.router.navigate(['/user/login']) && false;
+  }
+
+  private canView(): boolean {
+    return this.currentUser.loggedIn() && (this.permission.has('Root') || this.permission.has('ViewCart'));
+  }
+}
