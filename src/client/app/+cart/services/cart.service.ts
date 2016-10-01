aa385191b1bc9cba4571abe380d0efcfd72484ca
Ed Tramell <ed.tramell@wazeedigital.com>
2016-10-01 14:09:26 -0600

    INTERNAL: Update abstract ApiService examples
    - Specify "loading" option in actual CartService
      methods instead of in ApiService demo methods.
    - Demonstrate how ApiService could replace all optional
      parameters with one "options" object parameter.

--- src/client/app/+cart/services/cart.service.ts
+++ src/client/app/+cart/services/cart.service.ts
@@ -25,14 +25,14 @@ export class CartService {
   }
 
   public initializeData(): Observable<any> {
-    return this.apiServiceGet('orders', 'cart')
+    return this.apiServiceGet('orders', 'cart', { loading: true })
       .do(wholeCartResponse => this.store.replaceWith(wholeCartResponse))
       .map(() => { return {}; })
       .share();
   }
 
   public addProject(): void {
-    this.apiServicePost('orders', 'cart/project', this.createAddProjectRequestBody())
+    this.apiServicePost('orders', 'cart/project', this.createAddProjectRequestBody(), { loading: true })
       .subscribe(wholeCartResponse => {
         this.store.replaceWith(wholeCartResponse);
         this.cartSummaryService.loadCartSummary();
@@ -40,7 +40,7 @@ export class CartService {
   }
 
   public removeProject(project: Project): void {
-    this.apiServiceDelete('orders', `cart/project/${project.id}`)
+    this.apiServiceDelete('orders', `cart/project/${project.id}`, { loading: true })
       .subscribe(wholeCartResponse => {
         this.store.replaceWith(wholeCartResponse);
         this.cartSummaryService.loadCartSummary();
@@ -59,19 +59,33 @@ export class CartService {
   // Idea for ApiService enhancement.
   // If/when the ApiService abstracts away URL construction and JSON responses for us,
   // these methods will no longer be needed.
-  private apiServiceGet(apiService: string, urlEnding: string): Observable<any> {
-    return this.apiService.get(`/api/${apiService}/v1/${urlEnding}`, {}, true)
+  private get defaultApiServiceOptions(): ApiServiceOptions {
+    return { requestOptions: {}, loading: false };
+  }
+
+  private apiServiceGet(apiService: string, urlEnding: string, options: ApiServiceOptions = {}): Observable<any> {
+    options = Object.assign({}, this.defaultApiServiceOptions, options);
+    return this.apiService.get(`/api/${apiService}/v1/${urlEnding}`, options.requestOptions, options.loading)
       .map(response => response.json());
   }
 
-  private apiServicePost(apiService: string, urlEnding: string, body: string): Observable<any> {
-    return this.apiService.post(`/api/${apiService}/v1/${urlEnding}`, body, {}, true)
+  private apiServicePost(apiService: string, urlEnding: string, body: string, options: ApiServiceOptions = {}): Observable<any> {
+    options = Object.assign({}, this.defaultApiServiceOptions, options);
+    return this.apiService.post(`/api/${apiService}/v1/${urlEnding}`, body, options.requestOptions, options.loading)
       .map(response => response.json());
   }
 
-  private apiServiceDelete(apiService: string, urlEnding: string): Observable<any> {
-    return this.apiService.delete(`/api/${apiService}/v1/${urlEnding}`, {}, true)
+  private apiServiceDelete(apiService: string, urlEnding: string, options: ApiServiceOptions = {}): Observable<any> {
+    options = Object.assign({}, this.defaultApiServiceOptions, options);
+    return this.apiService.delete(`/api/${apiService}/v1/${urlEnding}`, options.requestOptions, options.loading)
       .map(response => response.json());
   }
   // END of ApiService abstractions.
+}
+
+import { RequestOptionsArgs } from '@angular/http';
+
+interface ApiServiceOptions {
+  requestOptions?: RequestOptionsArgs;
+  loading?: boolean;
 }
