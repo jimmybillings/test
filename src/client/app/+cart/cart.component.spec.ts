36cad12ef64beee60a7557a52fdc053cd72a94b7
Ed Tramell <ed.tramell@wazeedigital.com>
2016-10-05 15:05:35 -0600

    CRUX-943: Test new cart line item functionality
    - Also, "de-angularize" the specs by removing the injection
    system and directly instantiating the class under test
    with "new" and mock objects.

--- src/client/app/+cart/cart.component.spec.ts
+++ src/client/app/+cart/cart.component.spec.ts
@@ -1,49 +1,54 @@
-import { inject, TestBed, beforeEachProvidersArray } from '../imports/test.imports';
 import { Observable } from 'rxjs/Rx';
 
 import { CartComponent } from './cart.component';
-import { CartService } from './services/cart.service';
 
 export function main() {
   describe('Cart Component', () => {
-    const mockCartService = {
-      data: Observable.of({ someData: 'SOME_VALUE' }),
-      addProject: jasmine.createSpy('addProject'),
-      removeProject: jasmine.createSpy('removeProject')
-    };
+    let componentUnderTest: CartComponent;
+    let mockCartService: any;
+    let mockUiConfig: any;
 
     beforeEach(() => {
-      TestBed.configureTestingModule({
-        providers: [
-          ...beforeEachProvidersArray,
-          CartComponent,
-          { provide: CartService, useValue: mockCartService }
-        ]
-      });
+      mockCartService = {
+        data: Observable.of({ someData: 'SOME_VALUE' }),
+        addProject: jasmine.createSpy('addProject'),
+        removeProject: jasmine.createSpy('removeProject'),
+        updateProject: jasmine.createSpy('updateProject'),
+        moveLineItemTo: jasmine.createSpy('moveLineItemTo'),
+        cloneLineItem: jasmine.createSpy('cloneLineItem'),
+        removeLineItem: jasmine.createSpy('removeLineItem')
+      };
+
+      mockUiConfig = {
+        get: jasmine.createSpy('get').and.returnValue(Observable.of({ config: 'SOME_CONFIG' }))
+      };
+
+      componentUnderTest = new CartComponent(mockCartService, mockUiConfig);
     });
 
     describe('Initialization', () => {
-      let componentUnderTest: CartComponent;
-
-      beforeEach(inject([CartComponent], (cartComponent: CartComponent) => {
-        componentUnderTest = cartComponent;
-        componentUnderTest.ngOnInit();
-      }));
-
       it('connects to the CartService data', () => {
+        componentUnderTest.ngOnInit();
+
         componentUnderTest.cart.subscribe((cartData) => {
           expect(cartData.someData).toBe('SOME_VALUE');
         });
       });
+
+      it('gets the UI config specifically for the cart', () => {
+        componentUnderTest.ngOnInit();
+
+        expect(mockUiConfig.get).toHaveBeenCalledWith('cart');
+      });
+
+      it('caches the cart UI config', () => {
+        componentUnderTest.ngOnInit();
+
+        expect(componentUnderTest.config).toEqual('SOME_CONFIG');
+      });
     });
 
     describe('onNotification()', () => {
-      let componentUnderTest: CartComponent;
-
-      beforeEach(inject([CartComponent], (cartComponent: CartComponent) => {
-        componentUnderTest = cartComponent;
-      }));
-
       it('adds a project when notified with ADD_PROJECT', () => {
         componentUnderTest.onNotification({ type: 'ADD_PROJECT' });
 
@@ -55,6 +60,35 @@ export function main() {
         componentUnderTest.onNotification({ type: 'REMOVE_PROJECT', payload: mockProject });
 
         expect(mockCartService.removeProject).toHaveBeenCalledWith(mockProject);
+      });
+
+      it('updates a project when notified with UPDATE_PROJECT', () => {
+        let mockProject = {};
+        componentUnderTest.onNotification({ type: 'UPDATE_PROJECT', payload: mockProject });
+
+        expect(mockCartService.updateProject).toHaveBeenCalledWith(mockProject);
+      });
+
+      it('moves a line item when notified with MOVE_LINE_ITEM', () => {
+        let mockProject = {};
+        let mockLineItem = {};
+        componentUnderTest.onNotification({ type: 'MOVE_LINE_ITEM', payload: { lineItem: mockLineItem, otherProject: mockProject } });
+
+        expect(mockCartService.moveLineItemTo).toHaveBeenCalledWith(mockProject, mockLineItem);
+      });
+
+      it('clones a line item when notified with CLONE_LINE_ITEM', () => {
+        let mockLineItem = {};
+        componentUnderTest.onNotification({ type: 'CLONE_LINE_ITEM', payload: mockLineItem });
+
+        expect(mockCartService.cloneLineItem).toHaveBeenCalledWith(mockLineItem);
+      });
+
+      it('clones a line item when notified with REMOVE_LINE_ITEM', () => {
+        let mockLineItem = {};
+        componentUnderTest.onNotification({ type: 'REMOVE_LINE_ITEM', payload: mockLineItem });
+
+        expect(mockCartService.removeLineItem).toHaveBeenCalledWith(mockLineItem);
       });
     });
   });
