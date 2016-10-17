039120d2035395b06a1f8531c3aeaeaa348b6f4f
Ed Tramell <ed.tramell@wazeedigital.com>
2016-10-17 14:45:03 -0600

    CRUX-986: Update Authentication's ApiService calls

--- src/client/app/shared/services/authentication.data.service.spec.ts
+++ src/client/app/shared/services/authentication.data.service.spec.ts
@@ -24,24 +24,19 @@ export function main() {
       });
     });
 
-    it('Should create instance variables for http, apiconfig, apiUrls', inject([Authentication], (service: Authentication) => {
-      expect(service.api).toBeDefined();
-      expect(service._apiUrls).toBeDefined();
-    }));
-
     it('Should make a request to login a new user', inject([Authentication], (service: Authentication) => {
       service.create(setUser()).subscribe((res: any) => {
         expect(connection.request.url.split('.com')[1]).toBe('/api/identities/v1/login?siteName=core');
         expect(connection.request._body).toMatch(JSON.stringify(setUser()).slice(1, -1));
       });
-      connection.mockRespond(new Response(new ResponseOptions({body: setUser()})));
+      connection.mockRespond(new Response(new ResponseOptions({ body: setUser() })));
     }));
 
     it('Should make a request to destroy the login of a user', inject([Authentication], (service: Authentication) => {
       service.destroy().subscribe((res: any) => {
         expect(connection.request.url.split('.com')[1]).toBe('/api/identities/v1/invalidate?siteName=core');
       });
-      connection.mockRespond(new Response(new ResponseOptions({body: {}})));
+      connection.mockRespond(new Response(new ResponseOptions({ body: {} })));
     }));
   });
 
