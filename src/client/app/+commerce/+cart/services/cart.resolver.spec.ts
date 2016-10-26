f11e0fd5562703d1cfbad132a863ac957399c3e1
Ed Tramell <ed.tramell@wazeedigital.com>
2016-10-26 11:50:34 -0600

    CRUX-1042: Purify CartResolver spec

--- src/client/app/+commerce/+cart/services/cart.resolver.spec.ts
+++ src/client/app/+commerce/+cart/services/cart.resolver.spec.ts
@@ -1,35 +1,28 @@
-import { inject, TestBed, beforeEachProvidersArray } from '../../../imports/test.imports';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
 
 import { CartResolver } from './cart.resolver';
-import { CartService } from './cart.service';
 
 export function main() {
   describe('Cart Resolver', () => {
     const mockObservable = Observable.of({});
-    const mockCartService = {
+    const mockCartService: any = {
       initializeData: jasmine.createSpy('initializeData() spy').and.returnValue(mockObservable)
     };
     const mockRoute: ActivatedRouteSnapshot = undefined;
     const mockState: RouterStateSnapshot = undefined;
+    let resolverUnderTest: CartResolver;
 
     beforeEach(() => {
-      TestBed.configureTestingModule({
-        providers: [
-          ...beforeEachProvidersArray,
-          CartResolver,
-          { provide: CartService, useValue: mockCartService }
-        ]
-      });
+      resolverUnderTest = new CartResolver(mockCartService);
     });
 
     describe('resolve()', () => {
       let returnedObservable: Observable<any>;
 
-      beforeEach(inject([CartResolver], (cartResolver: CartResolver) => {
-        returnedObservable = cartResolver.resolve(mockRoute, mockState);
-      }));
+      beforeEach(() => {
+        returnedObservable = resolverUnderTest.resolve(mockRoute, mockState);
+      });
 
       it('tells the cart service to load data', () => {
         expect(mockCartService.initializeData).toHaveBeenCalled();
