e484dee14fa660a671ab4ae0ffeb56afabb2162e
James Billings <james_billings@me.com>
2016-10-31 15:53:57 -0600

    INTERNAL - linting

--- src/client/app/+user-management/services/user.data.service.ts
+++ src/client/app/+user-management/services/user.data.service.ts
@@ -11,19 +11,25 @@ import { Api } from '../../shared/interfaces/api.interface';
 export class User {
   constructor(private api: ApiService) { }
 
-  public create(user: Object): Observable<any> {
-    return this.api.post(Api.Identities, 'user/register', { body: user, loading: true });
-  }
-
   public get(): Observable<any> {
     return this.api.get(Api.Identities, 'user/currentUser');
   }
 
-  public forgotPassword(user: any): Observable<any> {
-    return this.api.post(Api.Identities, 'user/requestPasswordReset', { parameters: user, loading: true });
+  public create(user: Object): Observable<any> {
+    return this.api.post(Api.Identities, 'user/register',
+      { body: user, loading: true }
+    );
   }
 
-  public resetPassword(user: any, overridingToken:string): Observable<any> {
-    return this.api.post(Api.Identities, 'user/passwordReset', { body: user, overridingToken: overridingToken, loading: true});
+  public forgotPassword(user: any): Observable<any> {
+    return this.api.post(Api.Identities, 'user/requestPasswordReset',
+      { parameters: user, loading: true }
+    );
+  }
+
+  public resetPassword(user: any, overridingToken: string): Observable<any> {
+    return this.api.post(Api.Identities, 'user/passwordReset',
+      { body: user, overridingToken: overridingToken, loading: true }
+    );
   }
 }
