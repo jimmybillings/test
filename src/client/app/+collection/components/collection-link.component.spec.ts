4c65212d04b069f1b7a686ed1861116daf42ac31
James Billings <james_billings@me.com>
2016-11-01 11:17:06 -0600

    update spec ignore list

--- src/client/app/+collection/components/collection-link.component.spec.ts
+++ src/client/app/+collection/components/collection-link.component.spec.ts
@@ -1,12 +1,12 @@
 // TODO: Uncomment all commented lines after fixing 'require' issue.
-// import { CollectionLinkComponent } from './collection-link.component';
+import { CollectionLinkComponent } from './collection-link.component';
 
 export function main() {
   describe('Collection Link Component', () => {
-    // let componentUnderTest: CollectionLinkComponent;
+    let componentUnderTest: CollectionLinkComponent;
 
     beforeEach(() => {
-      // componentUnderTest = new CollectionLinkComponent();
+      componentUnderTest = new CollectionLinkComponent();
     });
 
     it('has no tests!', () => {
