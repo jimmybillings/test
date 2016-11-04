6e8193d6baa1273ddc07b4971be7883b446d70a4
James Billings <james_billings@me.com>
2016-11-04 08:54:06 -0600

    INTERNAL - specs for orders

--- src/client/app/+commerce/+order/services/order.resolver.spec.ts
+++ src/client/app/+commerce/+order/services/order.resolver.spec.ts
@@ -1,16 +1,43 @@
+import { RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
+
 import { OrderResolver } from './order.resolver';
 
 export function main() {
   describe('Order Resolver', () => {
+    // let mockUiConfig: any;
+    const mockObservable = Observable.of({});
+    const mockOrdersService: any = {
+      getOrder: jasmine.createSpy('getOrder').and.returnValue(mockObservable)
+    };
+
+    const mockRoute: any = {
+      params: Observable.of({ d: true, i: 0, n: 1, s: 'createdOn' }),
+      snapshot: { url: [{}] }
+    };
+
+    const mockState: RouterStateSnapshot = undefined;
+
     let resolverUnderTest: OrderResolver;
 
     beforeEach(() => {
-      resolverUnderTest = new OrderResolver(null);
+      resolverUnderTest = new OrderResolver(mockOrdersService);
     });
 
-    it('has no tests!', () => {
-      expect(true).toBe(true);
+    describe('resolve()', () => {
+      let returnedObservable: Observable<any>;
+
+      beforeEach(() => {
+        returnedObservable = resolverUnderTest.resolve(mockRoute, mockState);
+      });
+
+      it('tells the order service to get order data', () => {
+        expect(mockOrdersService.getOrder).toHaveBeenCalled();
+      });
+
+      it('returns the Observable returned by getOrder()', () => {
+        expect(returnedObservable).toBe(mockObservable);
+      });
     });
   });
 };
-
--- src/client/app/+commerce/+order/services/order.service.spec.ts
+++ src/client/app/+commerce/+order/services/order.service.spec.ts
@@ -1,16 +1,45 @@
+import { Observable } from 'rxjs/Rx';
+import { MockApiService, mockApiMatchers } from '../../../shared/mocks/mock-api.service';
+import { Api } from '../../../shared/interfaces/api.interface';
 import { OrderService } from './order.service';
 
 export function main() {
   describe('Order Service', () => {
     let serviceUnderTest: OrderService;
+    let mockOrderStore: any;
+    let mockApi: MockApiService;
 
     beforeEach(() => {
-      serviceUnderTest = new OrderService(null, null);
+      jasmine.addMatchers(mockApiMatchers);
+
+      mockOrderStore = {
+        data: Observable.of({ some: 'data' }),
+        state: { some: 'state' },
+        update: jasmine.createSpy('update')
+      };
+
+      mockApi = new MockApiService();
+      serviceUnderTest = new OrderService(mockApi.injector, mockOrderStore);
     });
 
-    it('has no tests!', () => {
-      expect(true).toBe(true);
+    describe('data getter', () => {
+      it('returns the data from the orders store', () => {
+        serviceUnderTest.data.subscribe(data => {
+          expect(data).toEqual({ some: 'data' });
+        });
+      });
     });
+
+    describe('getOrder()', () => {
+
+      it('calls the API service correctly', () => {
+        serviceUnderTest.getOrder(1).subscribe((res) => {
+          expect(mockApi.get).toHaveBeenCalledWithApi(Api.Orders);
+          expect(mockApi.get).toHaveBeenCalledWithEndpoint('order/1');
+          expect(mockOrderStore.update).toHaveBeenCalledWith(res);
+        });
+      });
+    });
+
   });
-};
-
+}
