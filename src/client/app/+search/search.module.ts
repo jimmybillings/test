a46d443fe71db5941d9132252146c721ee5e09ce
jhyde <jeff@jeffhyde.com>
2016-11-29 15:54:45 -0700

    CRUX-1140 added display for zero search results

--- src/client/app/+search/search.module.ts
+++ src/client/app/+search/search.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { SearchComponent } from './search.component';
 import { SharedModule } from '../shared/shared.module';
 import { FilterComponent } from './filter.component';
+import { NoResultsComponent } from './no-results.component';
 import { AssetData } from './services/asset.data.service';
 import { AssetStore } from './services/asset.store';
 
 @NgModule({
     imports: [SharedModule],
-    declarations: [SearchComponent, FilterComponent],
+    declarations: [SearchComponent, FilterComponent, NoResultsComponent],
     exports: [SearchComponent],
     providers: [AssetData, AssetStore]
 })
