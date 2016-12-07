65ffeaa89da86248a86c22bb920e024289978b52
Ed Tramell <ed.tramell@wazeedigital.com>
2016-12-07 13:26:53 -0700

    CRUX-673: Prevent invalid (backwards) date ranges

--- src/client/app/shared/components/wz-pikaday/wz-pikaday.directive.ts
+++ src/client/app/shared/components/wz-pikaday/wz-pikaday.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef} from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 var pikaday = require('pikaday');
 
 @Directive({
@@ -6,10 +6,29 @@ var pikaday = require('pikaday');
 })
 
 export class WzPikaDayDirective {
+  private picker: any;
 
   constructor(public element: ElementRef) {
-    new pikaday({ field: this.element.nativeElement });
+    this.picker = new pikaday({ field: this.element.nativeElement });
   }
 
+  @Input()
+  set maxDate(dateString: string) {
+    this.picker.setMaxDate(dateString ? this.convertToDateInstance(dateString) : null);
+  }
 
+  @Input()
+  set minDate(dateString: string) {
+    this.picker.setMinDate(dateString ? this.convertToDateInstance(dateString) : null);
+  }
+
+  private convertToDateInstance(dateString: string): Date {
+    // Thanks to commenter "jacquesporveau" at https://github.com/dbushell/Pikaday/issues/39.
+    const utcDate = new Date(dateString);
+    const offsetInMinutes = utcDate.getTimezoneOffset();
+    const offsetInMilliseconds = offsetInMinutes * 60 * 1000;
+    const fudgeFactor = 500;
+
+    return new Date(utcDate.getTime() + offsetInMilliseconds + fudgeFactor);
+  }
 }
