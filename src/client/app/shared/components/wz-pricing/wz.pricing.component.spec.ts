0c5ff6eca0ac4493c327a2f6869d4f80b0817981
rossedfort <ross.edfort@wazeedigital.com>
2016-12-15 09:10:19 -0700

    CRUX-1285 - add spec

---
+++ src/client/app/shared/components/wz-pricing/wz.pricing.component.spec.ts
@@ -0,0 +1,16 @@
+import { WzPricingComponent } from './wz.pricing.component';
+
+export function main() {
+  describe('Wz Pricing Component', () => {
+    let componentUnderTest: WzPricingComponent;
+
+    beforeEach(() => {
+      componentUnderTest = new WzPricingComponent();
+    });
+
+    it('has no tests!', () => {
+      expect(true).toBe(true);
+    });
+  });
+}
+    
