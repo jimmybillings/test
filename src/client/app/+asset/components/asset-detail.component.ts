886487dc9e32bcf46b3010fa213cab05e8becff5
Ed Tramell <ed.tramell@wazeedigital.com>
2016-12-15 13:55:32 -0700

    CRUX-1295: Fix unwanted sticky in/outs on clip change

--- src/client/app/+asset/components/asset-detail.component.ts
+++ src/client/app/+asset/components/asset-detail.component.ts
@@ -29,9 +29,13 @@ export class AssetDetailComponent implements OnChanges {
   @Output() addToCart = new EventEmitter();
   @ViewChild(MdMenuTrigger) trigger: MdMenuTrigger;
   private assetsArr: Array<number> = [];
+  public subclipMarkers: SubclipMarkers = {};
 
   ngOnChanges(changes: any): void {
-    if (changes.asset) this.parseNewAsset(changes.asset);
+    if (changes.asset) {
+      this.parseNewAsset(changes.asset);
+      this.subclipMarkers = {};
+    }
     if (changes.collection) {
       this.assetsArr = changes.collection.currentValue.assets.items.map((x: any) => x.assetId);
     }
