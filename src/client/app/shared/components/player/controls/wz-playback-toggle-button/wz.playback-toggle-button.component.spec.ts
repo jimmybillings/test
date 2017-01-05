4231b6cbb92711bda9267c7adf756bef25947d6b
Ed Tramell <ed.tramell@wazeedigital.com>
2017-01-04 19:21:38 -0700

    CRUX-1327, CRUX-1328: Add spec templates

---
+++ src/client/app/shared/components/player/controls/wz-playback-toggle-button/wz.playback-toggle-button.component.spec.ts
@@ -0,0 +1,16 @@
+import { WzPlaybackToggleButtonComponent } from './wz.playback-toggle-button.component';
+
+export function main() {
+  describe('Wz Playback Toggle Button Component', () => {
+    let componentUnderTest: WzPlaybackToggleButtonComponent;
+
+    beforeEach(() => {
+      componentUnderTest = new WzPlaybackToggleButtonComponent();
+    });
+
+    it('has no tests!', () => {
+      expect(true).toBe(true);
+    });
+  });
+}
+    
---
+++ src/client/app/shared/components/player/wz-player-controlbar/wz.player-controlbar.component.spec.ts
@@ -0,0 +1,16 @@
+import { WzPlayerControlbarComponent } from './wz.player-controlbar.component';
+
+export function main() {
+  describe('Wz Player Controlbar Component', () => {
+    let componentUnderTest: WzPlayerControlbarComponent;
+
+    beforeEach(() => {
+      componentUnderTest = new WzPlayerControlbarComponent();
+    });
+
+    it('has no tests!', () => {
+      expect(true).toBe(true);
+    });
+  });
+}
+    
