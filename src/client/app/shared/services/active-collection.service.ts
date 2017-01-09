70c05c2e6e30f8a16ad9bc6608e409b377c72153
rossedfort <ross.edfort@wazeedigital.com>
2017-01-09 11:50:10 -0700

    INTERNAL - fix bug where item doesnt get removed from collection

--- src/client/app/shared/services/active-collection.service.ts
+++ src/client/app/shared/services/active-collection.service.ts
@@ -58,7 +58,7 @@ export class ActiveCollectionService implements OnInit {
   public removeAsset(params: any): Observable<any> {
     let collection: Collection = params.collection;
     let uuid: any, assetToBeRemoved: any;
-    assetToBeRemoved = params.collection.assets.items.find((item: any) => item.assetId === params.asset.assetId);
+    assetToBeRemoved = params.collection.assets.items.find((item: any) => parseInt(item.assetId) === parseInt(params.asset.assetId));
     if (params.asset.uuid && assetToBeRemoved) {
       uuid = params.asset.uuid;
     } else {
@@ -69,7 +69,7 @@ export class ActiveCollectionService implements OnInit {
         Api.Identities,
         `collection/${collection.id}/removeAssets`,
         { body: { list: [{ assetId: params.asset.assetId, uuid: uuid }] } })
-      .do(response => this.store.remove(response['list'][0]));
+        .do(response => this.store.remove(response['list'][0]));
     } else {
       return Observable.of({});
     }
