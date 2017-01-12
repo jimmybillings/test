2fd0ca3dc457d4cef6b3195614e6c659e83d47ba
Ed Tramell <ed.tramell@wazeedigital.com>
2017-01-11 20:04:54 -0700

    INTERNAL: Fix some bad imports

--- src/client/app/shared/components/player/wz.player-state.service.ts
+++ src/client/app/shared/components/player/wz.player-state.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/Rx';
 
 import { Frame } from 'wazee-frame-formatter';
 import { WzPlayerState, WzPlayerStateChanges } from './wz.player.interface';
