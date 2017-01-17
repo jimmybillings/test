60668065aa771f75d2157600a51d24a24f0eff9b
Ross Edfort <redfort@Webopss-MacBook-Pro-89.local>
2017-01-17 16:09:06 -0700

    CRUX-1307 - add more specs

--- src/client/app/shared/components/player/wz-player/wz.player.component.spec.ts
+++ src/client/app/shared/components/player/wz-player/wz.player.component.spec.ts
@@ -11,7 +11,7 @@ export function main() {
   // ---- In advanced mode
   // ------ Asset Setter
 
-  describe('Player Component', () => {
+  fdescribe('Player Component', () => {
     let componentUnderTest: WzPlayerComponent, mockElementRef: ElementRef, mockZone: NgZone, mockPlayer: any;
 
     function JwPlayer(nativeElement: any) {
@@ -27,6 +27,8 @@ export function main() {
       mockPlayer = {};
       // Mock Player public interface
       mockPlayer.setup = jasmine.createSpy('setup');
+      mockPlayer.remove = jasmine.createSpy('remove');
+      mockPlayer.getState = jasmine.createSpy('getState').and.returnValue('paused');
       mockPlayer.play = jasmine.createSpy('play').and.returnValue(mockPlayer);
       mockPlayer.pause = jasmine.createSpy('pause').and.returnValue(mockPlayer);
       mockPlayer.seek = jasmine.createSpy('seek').and.callFake(() => {
@@ -65,12 +67,12 @@ export function main() {
         seek: [],
         time: []
       };
-      mockPlayer.emit = (eventName: string) => {
+      mockPlayer.emit = (eventName: string, args?: any) => {
         mockPlayer.onCallbacks[eventName].forEach((callback: Function) => {
-          callback();
+          callback(args);
         });
         mockPlayer.onceCallbacks[eventName].forEach((callback: Function) => {
-          callback();
+          callback(args);
         });
         mockPlayer.onceCallbacks[eventName] = [];
       };
@@ -135,6 +137,17 @@ export function main() {
         it('playRange() should throw a new TypeError', () => {
           expect(() => { componentUnderTest.playRange(1.234, 5.678); }).toThrowError('Must be in advanced mode to play subclip.');
         });
+
+        it('should reset the player if it already exists when a new asset is set', () => {
+          componentUnderTest.asset = {
+            resourceClass: 'AnotherNotImage',
+            clipThumbnailUrl: 'anotherClipThumbnailUrl',
+            clipUrl: 'anotherClipUrl'
+          };
+
+          expect(mockPlayer.pause).toHaveBeenCalled();
+          expect(mockPlayer.remove).toHaveBeenCalled();
+        });
       });
 
       describe('in advanced mode', () => {
@@ -161,9 +174,88 @@ export function main() {
             });
           });
 
+          it('should reset the player if it already exists when a new asset is set', () => {
+            spyOn(componentUnderTest.stateUpdate, 'emit');
 
-          xit('should handleDurationEvent()', () => {
-            expect(mockPlayer.once).toHaveBeenCalled();
+            componentUnderTest.asset = {
+              resourceClass: 'AnotherNotImage',
+              clipThumbnailUrl: 'anotherClipThumbnailUrl',
+              clipUrl: 'anotherClipUrl'
+            };
+
+            expect(mockPlayer.pause).toHaveBeenCalled();
+            expect(mockPlayer.remove).toHaveBeenCalled();
+            expect(componentUnderTest.stateUpdate.emit).toHaveBeenCalledWith({ duration: undefined, currentTime: 0 });
+          });
+
+          it('should call handleDurationEvent which emits the stateUpdate event with the duration', () => {
+            spyOn(componentUnderTest.stateUpdate, 'emit');
+
+            mockPlayer.emit('time', { duration: 123 });
+
+            expect(mockPlayer.once).toHaveBeenCalledWith('time', jasmine.any(Function));
+            expect(componentUnderTest.stateUpdate.emit).toHaveBeenCalledWith({ duration: 123 });
+          });
+
+          it('should call handleTimeEvents which emits the stateUpdate event with the currentTime', () => {
+            spyOn(componentUnderTest.stateUpdate, 'emit');
+
+            mockPlayer.emit('time', { position: 456 });
+
+            expect(mockPlayer.on).toHaveBeenCalledWith('time', jasmine.any(Function));
+            expect(componentUnderTest.stateUpdate.emit).toHaveBeenCalledWith({ currentTime: 456 });
+          });
+
+          describe('handlePlaybackStateEvents()', () => {
+            beforeEach(() => {
+              spyOn(componentUnderTest.stateUpdate, 'emit');
+            });
+
+
+            it('should call .on() with \'play\'', () => {
+              expect(mockPlayer.on).toHaveBeenCalledWith('play', jasmine.any(Function));
+              mockPlayer.emit('play');
+              expect(componentUnderTest.stateUpdate.emit).toHaveBeenCalledWith({ playing: true });
+            });
+
+            it('should call .on() with \'pause\'', () => {
+              expect(mockPlayer.on).toHaveBeenCalledWith('pause', jasmine.any(Function));
+              mockPlayer.emit('pause');
+              expect(componentUnderTest.stateUpdate.emit).toHaveBeenCalledWith({ playing: false });
+            });
+
+            it('should call .on() with \'complete\'', () => {
+              expect(mockPlayer.on).toHaveBeenCalledWith('complete', jasmine.any(Function));
+              mockPlayer.emit('complete');
+              expect(componentUnderTest.stateUpdate.emit).toHaveBeenCalledWith({ playing: false });
+            });
+          });
+
+          describe('preventAutoplayAfterSeek()', () => {
+            beforeEach(() => {
+              componentUnderTest.mode = 'advanced';
+              componentUnderTest.asset = {
+                resourceClass: 'NotImage',
+                clipThumbnailUrl: undefined,
+                clipUrl: 'clipUrl'
+              };
+            });
+
+            it('should call .on() with \'seek\'', () => {
+              expect(mockPlayer.on).toHaveBeenCalledWith('seek', jasmine.any(Function));
+            });
+
+            it('should call .getState() on the player in the callback', () => {
+              mockPlayer.emit('seek');
+
+              expect(mockPlayer.getState).toHaveBeenCalled();
+            });
+
+            it('should call .pause() on the player if the player was paused', () => {
+              mockPlayer.emit('seeked');
+
+              expect(mockPlayer.pause).toHaveBeenCalled();
+            });
           });
         });
 
@@ -171,6 +263,32 @@ export function main() {
           it('should call .off() on the player with \'time\'', () => {
             componentUnderTest.playRange(1.234, 5.678);
 
+            expect(mockPlayer.off).toHaveBeenCalledWith('time');
+          });
+
+          it('should call .on() on the player with \'time\'', () => {
+            componentUnderTest.playRange(1.234, 5.678);
+
+            expect(mockPlayer.on).toHaveBeenCalledWith('time', jasmine.any(Function));
+          });
+
+          it('should pause the player and remove .off handler if the position is === the endpoint', () => {
+            spyOn(componentUnderTest.stateUpdate, 'emit');
+            componentUnderTest.playRange(1.234, 5.678);
+
+            mockPlayer.emit('time', { position: 5.678 });
+
+            expect(mockPlayer.pause).toHaveBeenCalled();
+            expect(mockPlayer.off).toHaveBeenCalledWith('time');
+          });
+
+          it('should not pause the player and remove .off handler if the position is > the endpoint', () => {
+            spyOn(componentUnderTest.stateUpdate, 'emit');
+            componentUnderTest.playRange(1.234, 5.678);
+
+            mockPlayer.emit('time', { position: 1 });
+
+            expect(mockPlayer.pause).toHaveBeenCalled();
             expect(mockPlayer.off).toHaveBeenCalledWith('time');
           });
         });
