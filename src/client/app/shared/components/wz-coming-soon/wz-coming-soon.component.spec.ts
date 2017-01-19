34a6f8858f79b6ec4677a456a79add6ce1a66ab8
James Billings <james_billings@me.com>
2017-01-18 23:07:19 -0700

    add missing specs

---
+++ src/client/app/shared/components/wz-coming-soon/wz-coming-soon.component.spec.ts
@@ -0,0 +1,15 @@
+import { WzComingSoonComponent } from './wz-coming-soon.component';
+
+export function main() {
+  describe('Wz Coming Soon Component', () => {
+    let componentUnderTest: WzComingSoonComponent;
+
+    beforeEach(() => {
+      componentUnderTest = new WzComingSoonComponent();
+    });
+
+    it('has no tests!', () => {
+      expect(true).toBe(true);
+    });
+  });
+}
