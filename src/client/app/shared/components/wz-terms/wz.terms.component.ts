36115c8e38bc82f46764306330a3334f01c2ed9b
Ross Edfort <ross.edfort@wazeedigital.com>
2017-02-06 11:03:55 -0700

    CRUX-1474 - tos checkbox maintains state when opening dialog
    - previously, the terms dialog had an agree button
    - when a user clicked it, the dialog closed and the checkbox toggled
    - now the button says 'close' and the checkbox remains unaffected
    - *I got the OK from PO's to change this*

--- src/client/app/shared/components/wz-terms/wz.terms.component.ts
+++ src/client/app/shared/components/wz-terms/wz.terms.component.ts
@@ -9,10 +9,4 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class WzTermsComponent {
   @Input() terms: any;
   @Input() dialog: any;
-
-  public agreeToTerms(): void {
-    let agreeCheckbox = <HTMLFormElement>document.querySelector('.md-checkbox-layout');
-    if (agreeCheckbox) agreeCheckbox.click();
-    this.dialog.close();
-  }
 }
