2218e01b0974f78966cc6728a0c96c1bd945b366
Ed Tramell <ed.tramell@wazeedigital.com>
2017-02-24 13:44:52 -0700

    CRUX-1519: Add tests to GalleryViewStore's spec

--- src/client/app/+gallery-view/services/gallery-view.store.spec.ts
+++ src/client/app/+gallery-view/services/gallery-view.store.spec.ts
@@ -1,15 +1,111 @@
-import { GalleryViewStore } from './gallery-view.store';
+import { Observable } from 'rxjs/Rx';
+
+import { addStandardReducerTestsFor } from '../../shared/tests/reducer';
+import { gallery, GalleryViewStore } from './gallery-view.store';
+import { Gallery } from '../gallery-view.interface';
 
 export function main() {
+  const initialState: Gallery = {
+    results: [],
+    numberOfLevels: 0,
+    path: []
+  };
+
+  describe('gallery reducer', () => {
+    describe('REPLACE_GALLERY', () => {
+      addStandardReducerTestsFor(gallery, 'REPLACE_GALLERY', initialState);
+
+      it('returns payload when current state is passed in', () => {
+        expect(gallery(initialState, { type: 'REPLACE_GALLERY', payload: { property1: 'new', other: 'stuff' } }))
+          .toEqual({ property1: 'new', other: 'stuff' });
+      });
+
+      it('returns payload when current state is not passed in', () => {
+        expect(gallery(undefined, { type: 'REPLACE_GALLERY', payload: { some: 'payload' } }))
+          .toEqual({ some: 'payload' });
+      });
+
+      it('returns initial state when payload is not passed in', () => {
+        expect(gallery({ property1: 'existing1', property2: 'existing2' } as any, { type: 'REPLACE_GALLERY' }))
+          .toEqual(initialState);
+      });
+    });
+  });
+
   describe('Gallery View Store', () => {
     let storeUnderTest: GalleryViewStore;
+    let mockStore: any;
 
     beforeEach(() => {
-      storeUnderTest = new GalleryViewStore(null);
+      mockStore = {
+        select: jasmine.createSpy('select').and.returnValue(Observable.of({ someKey: 'someValue' })),
+        dispatch: jasmine.createSpy('dispatch')
+      };
+
+      storeUnderTest = new GalleryViewStore(mockStore);
     });
 
-    it('has no tests!', () => {
-      expect(true).toBe(true);
+    describe('data getter', () => {
+      it('accesses the right part of the global store', () => {
+        storeUnderTest.data.subscribe();
+        expect(mockStore.select).toHaveBeenCalledWith('gallery');
+      });
+
+      it('returns the expected data', () => {
+        storeUnderTest.data.subscribe(data => {
+          expect(data).toEqual({ someKey: 'someValue' });
+        });
+      });
+    });
+
+    describe('state getter', () => {
+      it('should return the state', () => {
+        expect(storeUnderTest.state).toEqual({ someKey: 'someValue' });
+      });
+    });
+
+    describe('replaceWith()', () => {
+      it('dispatches REPLACE_GALLERY with the expected payload', () => {
+        const results = [{ id: 3, name: 'Name 3', resultCount: 42, hasChildren: false }];
+        const path = [{ ids: [1, 2], names: ['Name 1', 'Name 2'] }];
+
+        storeUnderTest.replaceWith(results, path);
+
+        expect(mockStore.dispatch).toHaveBeenCalledWith(
+          { type: 'REPLACE_GALLERY', payload: { results: results, numberOfLevels: 1, path: path } }
+        );
+      });
+
+      it('calculates the correct number of levels for a complex results set', () => {
+        const results = [
+          {
+            id: 3, name: 'Name 3', resultCount: 42, hasChildren: true, children: [
+              {
+                id: 4, name: 'Name 4_1', resultCount: 17, hasChildren: true, children: [
+                  { id: 5, name: 'Name 5_1', resultCount: 2, hasChildren: false },
+                  { id: 5, name: 'Name 5_2', resultCount: 46, hasChildren: false },
+                  { id: 5, name: 'Name 5_3', resultCount: 23, hasChildren: false }
+                ]
+              },
+              {
+                id: 4, name: 'Name 4_2', resultCount: 99, hasChildren: true, children: [
+                  { id: 5, name: 'Name 5_1', resultCount: 2, hasChildren: false },
+                  { id: 5, name: 'Name 5_2', resultCount: 46, hasChildren: false },
+                  { id: 5, name: 'Name 5_3', resultCount: 23, hasChildren: false }
+                ]
+              }
+            ]
+          }
+        ];
+
+        const path = [{ ids: [1, 2], names: ['Name 1', 'Name 2'] }];
+
+        storeUnderTest.replaceWith(results, path);
+
+        expect(mockStore.dispatch).toHaveBeenCalledWith(
+          { type: 'REPLACE_GALLERY', payload: { results: results, numberOfLevels: 3, path: path } }
+        );
+      });
     });
   });
 }
--- src/client/app/+gallery-view/services/gallery-view.store.ts
+++ src/client/app/+gallery-view/services/gallery-view.store.ts
@@ -4,16 +4,10 @@ import { Store, ActionReducer, Action } from '@ngrx/store';
 
 import { Gallery, GalleryResults, GalleryPath } from '../gallery-view.interface';
 
-const emptyGallery: Gallery = {
-  results: [],
-  numberOfLevels: 0,
-  path: []
-};
-
-export const gallery: ActionReducer<Gallery> = (state: Gallery = emptyGallery, action: Action) => {
+export const gallery: ActionReducer<Gallery> = (state: Gallery = initialState(), action: Action) => {
   switch (action.type) {
-    case 'UPDATE_GALLERY':
-      return Object.assign({}, action.payload);
+    case 'REPLACE_GALLERY':
+      return Object.assign({}, action.payload ? action.payload : initialState());
 
     default:
       return state;
@@ -28,20 +22,28 @@ export class GalleryViewStore {
     return this.store.select('gallery');
   }
 
-  public replaceWith(results: GalleryResults, path: GalleryPath): void {
-    this.store.dispatch({
-      type: 'UPDATE_GALLERY',
-      payload: { results: results, numberOfLevels: this.numberOfLevelsIn(results), path: path }
-    });
-  }
-
   public get state(): Gallery {
     let state: Gallery;
     this.data.take(1).subscribe(galleryData => state = galleryData);
     return state;
   }
 
+  public replaceWith(results: GalleryResults, path: GalleryPath): void {
+    this.store.dispatch({
+      type: 'REPLACE_GALLERY',
+      payload: { results: results, numberOfLevels: this.numberOfLevelsIn(results), path: path }
+    });
+  }
+
   private numberOfLevelsIn(results: GalleryResults): number {
     return results ? 1 + Math.max(...results.map(result => this.numberOfLevelsIn(result.children))) : 0;
   }
 }
+
+function initialState(): Gallery {
+  return {
+    results: [],
+    numberOfLevels: 0,
+    path: []
+  };
+};
