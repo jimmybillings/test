097bd6dfeaa63ceadc02e7f5fa71ba6ae90bf32b
jhyde <jeff@jeffhyde.com>
2017-02-27 10:20:53 -0700

    CRUX-1519 UI root level carousel for gallery view

--- src/client/app/+gallery-view/components/two-level-view.component.ts
+++ src/client/app/+gallery-view/components/two-level-view.component.ts
@@ -11,11 +11,20 @@ import { Gallery, GalleryResult, GalleryNavigationEvent } from '../gallery-view.
 export class TwoLevelViewComponent {
   @Input() public data: Gallery;
   @Output() public navigate: EventEmitter<GalleryNavigationEvent> = new EventEmitter<GalleryNavigationEvent>();
+  public activeItemHovered: boolean = false;
+  public activeRow: number;
 
   public onClick(result: GalleryResult, child: GalleryResult) {
     this.navigate.emit({
       pathSegment: { ids: [result.id, child.id], names: [result.name, child.name] },
       method: child.hasMore ? 'nextLevel' : 'search'
     });
+  }
+  public onMouseOver(child: GalleryResult, index: number): void {
+    this.activeItemHovered = child.resultCount > 0 ? true : false;
+    this.activeRow = index;
+  }
+  public onMouseOut(child: GalleryResult): void {
+    this.activeItemHovered = false;
   }
 }
