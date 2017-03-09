e834542e59492679fb6192bc5fa8fb29420b5c02
Ross Edfort <ross.edfort@wazeedigital.com>
2017-03-09 11:02:33 -0700

    CRUX-1426 - fix broken spec

--- src/client/app/shared/services/cart.service.spec.ts
+++ src/client/app/shared/services/cart.service.spec.ts
@@ -327,7 +327,9 @@ export function main() {
 
         expect(mockApi.put).toHaveBeenCalledWithApi(Api.Orders);
         expect(mockApi.put).toHaveBeenCalledWithEndpoint('cart/update/lineItem/456');
-        expect(mockApi.put).toHaveBeenCalledWithBody({ id: '456', price: 0, rightsManaged: false, selectedTranscodeTarget: '1080i' });
+        expect(mockApi.put).toHaveBeenCalledWithBody({
+          id: '456', price: 0, rightsManaged: 'Rights Managed', selectedTranscodeTarget: '1080i'
+        });
       });
 
       it('replaces the cart store with the response', () => {
