1015914f599f4fdb88157dd3d40839ed44068c24
Ross Edfort <ross.edfort@wazeedigital.com>
2017-03-09 10:51:39 -0700

    CRUX-1426 - fix calculate price in the cart
    - previously the rightsManaged property on line items was a boolean
    - it was recently changed to a string, which caused the UI to break

--- src/client/app/shared/interfaces/cart.interface.ts
+++ src/client/app/shared/interfaces/cart.interface.ts
@@ -19,7 +19,7 @@ export interface LineItem {
   id: string;
   asset?: Asset;
   price: number;
-  rightsManaged: boolean;
+  rightsManaged: string;
   transcodeTargets?: Array<string>;
   selectedTranscodeTarget?: string;
   attributes?: Array<any>;
