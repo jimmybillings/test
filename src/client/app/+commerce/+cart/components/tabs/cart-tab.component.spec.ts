1f6f2a41f4447904a62bf227797c2f624b4b7b68
Ed Tramell <ed.tramell@wazeedigital.com>
2017-03-10 19:43:51 -0700

    CRUX-1615: Use WzSubclipEditorComponent in CartTabComponent

--- src/client/app/+commerce/+cart/components/tabs/cart-tab.component.spec.ts
+++ src/client/app/+commerce/+cart/components/tabs/cart-tab.component.spec.ts
@@ -1,8 +1,7 @@
 import { Observable } from 'rxjs/Rx';
 import { CartTabComponent } from './cart-tab.component';
 import { EditProjectComponent } from '../edit-project.component';
-import { WzAdvancedPlayerComponent } from
-  '../../../../shared/modules/wz-player/components/wz-advanced-player/wz.advanced-player.component';
+import { WzSubclipEditorComponent } from '../../../../shared/components/wz-subclip-editor/wz.subclip-editor.component';
 import { WzPricingComponent } from '../../../../shared/components/wz-pricing/wz.pricing.component';
 
 export function main() {
@@ -267,7 +266,7 @@ export function main() {
         componentUnderTest.onNotification({ type: 'EDIT_LINE_ITEM_MARKERS', payload: { asset: mockAsset } });
 
         expect(mockAssetService.getClipPreviewData).toHaveBeenCalledWith(1234);
-        expect(mockDialog.open).toHaveBeenCalledWith(WzAdvancedPlayerComponent, { width: '544px' });
+        expect(mockDialog.open).toHaveBeenCalledWith(WzSubclipEditorComponent, { width: '544px' });
       });
 
       describe('calls openPricingDialog with SHOW_PRICING_DIALOG', () => {
