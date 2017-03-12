f7513adf9e63d9359c1a43f1141eae5e6b0ffd43
Ed Tramell <ed.tramell@wazeedigital.com>
2017-03-12 17:29:59 -0600

    CRUX-1615: Absorb SubclipEditActionsComponent into WzSubclipEditorComponent

--- src/client/app/shared/components/wz-subclip-editor/wz.subclip-editor.component.ts
+++ src/client/app/shared/components/wz-subclip-editor/wz.subclip-editor.component.ts
@@ -13,13 +13,24 @@ import { Frame } from 'wazee-frame-formatter';
       (onUpdateSubclipData)="onPlayerSubclipUpdate($event)">
     </wz-advanced-player>
 
-    <wz-subclip-edit-actions layout="row" layout-align="end center"
-      [hasStoredMarkers]="asset.timeStart != undefined"
-      [dialog]="dialog"
-      [inMarkerFrame]="inMarkerFrame"
-      [outMarkerFrame]="outMarkerFrame"
-      (saveMarkers)="onSaveMarkers($event)">
-    </wz-subclip-edit-actions>
+    <section layout="row" layout-align="end">
+      <button md-button color="primary" (click)="dialog.close()">
+        {{ 'ASSET.SAVE_SUBCLIP.EDIT_ACTIONS.CANCEL_BTN_LABEL' | translate }}
+      </button>
+
+      <button md-button class="is-outlined" color="primary"
+        *ngIf="!showRemoveMarkers()"
+        [disabled]="!inMarkerFrame || !outMarkerFrame"
+        (click)="onSave()">
+        {{ 'ASSET.SAVE_SUBCLIP.EDIT_ACTIONS.SAVE_BTN_LABEL' | translate }}
+      </button>
+      
+      <button md-button class="is-outlined" color="accent"
+        *ngIf="showRemoveMarkers()"
+        (click)="onRemove()">
+        {{ 'ASSET.SAVE_SUBCLIP.EDIT_ACTIONS.REMOVE_BTN_LABEL' | translate }}
+      </button>
+    </section>
   `
 })
 
@@ -37,7 +48,15 @@ export class WzSubclipEditorComponent {
     this.outMarkerFrame = event.out;
   }
 
-  public onSaveMarkers(event: any): void {
-    this.onSubclip.emit(event);
+  public showRemoveMarkers(): boolean {
+    return this.asset.timeStart && !this.inMarkerFrame && !this.outMarkerFrame;
+  }
+
+  public onSave(): void {
+    this.onSubclip.emit({ in: this.inMarkerFrame.frameNumber, out: this.outMarkerFrame.frameNumber });
+  }
+
+  public onRemove(): void {
+    this.onSubclip.emit({ in: undefined, out: undefined });
   }
 }
