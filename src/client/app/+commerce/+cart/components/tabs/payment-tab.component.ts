89eb0bd1761aa600e36db8d5a4757fa41e21598d
Ross Edfort <ross.edfort@wazeedigital.com>
2017-03-15 13:36:27 -0600

    CRUX-1302 - purchase on credit button honors account and user options

--- src/client/app/+commerce/+cart/components/tabs/payment-tab.component.ts
+++ src/client/app/+commerce/+cart/components/tabs/payment-tab.component.ts
@@ -52,6 +52,31 @@ export class PaymentTabComponent extends Tab implements OnInit {
       });
   }
 
+  public get userCanPurchaseOnCredit(): Observable<boolean> {
+    return this.cartService.data.map((data: any) => {
+      let options: any = data.orderInProgress.purchaseOptions;
+      if (data.orderInProgress.selectedAddress.type === 'user') {
+        return options.purchaseOnCredit;
+      } else {
+        if (options.purchaseOnCredit) {
+          if (options.creditExemption) {
+            return data.cart.total > parseInt(options.creditExemption);
+          } else {
+            return true;
+          }
+        } else {
+          return options.purchaseOnCredit;
+        }
+      }
+    });
+  }
+
+  public purchaseOnCredit(): void {
+    this.cartService.purchaseOnCredit().take(1).subscribe((order: any) => {
+      console.log(order);
+    });
+  }
+
   private loadStripe() {
     const stripeScript = 'https://js.stripe.com/v2/';
     var scripts = document.getElementsByTagName('script');
