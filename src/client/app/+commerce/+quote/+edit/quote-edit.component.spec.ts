76156bac23cfe607599f5642a6851b067c565b1b
Ed Tramell <ed.tramell@wazeedigital.com>
2017-04-26 17:49:02 -0600

    CRUX-1224: Add unit tests for add/remove quote fees

--- src/client/app/+commerce/+quote/+edit/quote-edit.component.spec.ts
+++ src/client/app/+commerce/+quote/+edit/quote-edit.component.spec.ts
@@ -3,13 +3,42 @@ import { QuoteEditComponent } from './quote-edit.component';
 export function main() {
   describe('Quote Edit Component', () => {
     let componentUnderTest: QuoteEditComponent;
+    let mockQuoteEditService: any;
 
     beforeEach(() => {
-      componentUnderTest = new QuoteEditComponent(null, null, null, null, null, null, null, null, null, null, null, null);
+      mockQuoteEditService = {
+        addFeeTo: jasmine.createSpy('addFeeTo'),
+        removeFee: jasmine.createSpy('removeFee')
+      };
+
+      componentUnderTest =
+        new QuoteEditComponent(null, mockQuoteEditService, null, null, null, null, null, null, null, null, null, null);
     });
 
-    it('has no tests!', () => {
-      expect(true).toBe(true);
+    describe('onNotification()', () => {
+      describe('ADD_QUOTE_FEE', () => {
+        it('calls the addFeeTo() service method', () => {
+          componentUnderTest.onNotification(
+            { type: 'ADD_QUOTE_FEE', payload: { project: { some: 'project' }, fee: { some: 'fee' } } }
+          );
+
+          expect(mockQuoteEditService.addFeeTo).toHaveBeenCalledWith({ some: 'project' }, { some: 'fee' });
+        });
+
+        it('throws an error if the message doesn\'t have a payload', () => {
+          expect(() => componentUnderTest.onNotification({ type: 'ADD_QUOTE_FEE' })).toThrowError();
+        });
+      });
+
+      describe('REMOVE_QUOTE_FEE', () => {
+        it('calls the removeFee() service method', () => {
+          componentUnderTest.onNotification(
+            { type: 'REMOVE_QUOTE_FEE', payload: { some: 'fee' } }
+          );
+
+          expect(mockQuoteEditService.removeFee).toHaveBeenCalledWith({ some: 'fee' });
+        });
+      });
     });
   });
 }
