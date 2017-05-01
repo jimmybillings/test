6acb92c5a7166ea89ceb365c93d24456e2d3ec22
James Billings <james_billings@me.com>
2017-05-01 09:29:49 -0600

    INTERNAL update dialog module

--- src/client/app/shared/modules/wz-dialog/wz.dialog.module.ts
+++ src/client/app/shared/modules/wz-dialog/wz.dialog.module.ts
@@ -1,37 +1,36 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MaterialModule } from '@angular/material';
-import { TranslateModule } from '@ngx-translate/core';
+// import { NgModule, ModuleWithProviders } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { MaterialModule } from '@angular/material';
+// import { TranslateModule } from '@ngx-translate/core';
 
-import { WzFormModule } from '../wz-form/wz-form.module';
+// import { WzFormModule } from '../wz-form/wz-form.module';
 
-// Wrapper service
-import { WzDialogService } from './services/wz.dialog.service';
+// // Wrapper service
+// import { WzDialogService } from './services/wz.dialog.service';
 
-// Dialog types
-import {
-  WzConfirmationDialogComponent,
-  WzNotificationDialogComponent,
-  WzFormDialogComponent,
-} from './components/index';
 
-@NgModule({
-  imports: [
-    CommonModule,
-    MaterialModule,
-    TranslateModule,
-    WzFormModule
-  ],
-  declarations: [
-    WzNotificationDialogComponent,
-    WzConfirmationDialogComponent,
-    WzFormDialogComponent
-  ],
-  entryComponents: [
-    WzNotificationDialogComponent,
-    WzConfirmationDialogComponent,
-    WzFormDialogComponent
-  ],
-  providers: [WzDialogService]
-})
-export class WzDialogModule { }
+// // Dialog types
+// import { WzNotificationDialogComponent } from './components/wz.notification-dialog.component';
+// import { WzFormDialogComponent } from './components/wz.form-dialog.component';
+// import { WzPricingDialogComponent } from './components/wz.pricing-dialog.component';
+
+// @NgModule({
+//   imports: [
+//     CommonModule,
+//     MaterialModule,
+//     TranslateModule,
+//     WzFormModule
+//   ],
+//   declarations: [
+//     WzNotificationDialogComponent,
+//     WzFormDialogComponent,
+//     WzPricingDialogComponent
+//   ],
+//   entryComponents: [
+//     WzNotificationDialogComponent,
+//     WzFormDialogComponent,
+//     WzPricingDialogComponent
+//   ],
+//   providers: [WzDialogService]
+// })
+// export class WzDialogModule { }
