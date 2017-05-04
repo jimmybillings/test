9d0f2426106e3c62df785bb7bfae2195164da671
Ross Edfort <ross.edfort@wazeedigital.com>
2017-05-04 11:53:05 -0600

    INTERNAL - add specs for quote edit service

--- src/client/app/shared/services/quote-edit.service.spec.ts
+++ src/client/app/shared/services/quote-edit.service.spec.ts
@@ -6,18 +6,309 @@ import { Observable } from 'rxjs/Observable';
 export function main() {
   describe('Quote Edit Service', () => {
     let serviceUnderTest: QuoteEditService, mockApi: MockApiService, mockQuoteStore: any, mockFeeConfigStore: any;
+    let mockState: any = { data: { id: 3, ownerUserId: 10, total: 90, subTotal: 100, projects: [{ name: 'Project A' }] } };
 
     beforeEach(() => {
       mockApi = new MockApiService();
       mockQuoteStore = {
-        data: Observable.of({ data: { id: 3, ownerUserId: 10 } }),
-        state: { data: { id: 3, ownerUserId: 10 } },
+        data: Observable.of(mockState),
+        state: mockState,
         replaceQuote: jasmine.createSpy('replaceQuote'),
         updateQuote: jasmine.createSpy('updateQuote')
       };
       mockFeeConfigStore = {};
       jasmine.addMatchers(mockApiMatchers);
       serviceUnderTest = new QuoteEditService(mockQuoteStore, mockFeeConfigStore, mockApi.injector);
+    });
+
+    describe('Store Accessors', () => {
+      describe('get data', () => {
+        it('should return an observable of the data from the store', () => {
+          serviceUnderTest.data.subscribe(d => expect(d).toEqual(mockState));
+        });
+      });
+
+      describe('get state', () => {
+        it('shold return the state from the store', () => {
+          expect(serviceUnderTest.state).toEqual(mockState);
+        });
+      });
+
+      describe('get quote', () => {
+        it('should return an observable of the quote in the store', () => {
+          serviceUnderTest.quote.subscribe(d => expect(d).toEqual(mockState.data));
+        });
+      });
+
+      describe('get projects', () => {
+        it('should return an observable of the projects in the store', () => {
+          serviceUnderTest.projects.subscribe(d => expect(d).toEqual(mockState.data.projects));
+        });
+      });
+
+      describe('get total', () => {
+        it('shold return an observable of the total in the store', () => {
+          serviceUnderTest.total.subscribe(d => expect(d).toBe(90));
+        });
+      });
+
+      describe('get subTotal', () => {
+        it('shold return an observable of the subTotal in the store', () => {
+          serviceUnderTest.subTotal.subscribe(d => expect(d).toBe(100));
+        });
+      });
+
+      describe('get hasHAssets', () => {
+        it('should return false if the quote does not have the itemCount property', () => {
+          mockQuoteStore = { data: Observable.of({ data: {} }) };
+          new QuoteEditService(mockQuoteStore, null, null).hasAssets.subscribe(d => expect(d).toBe(false));
+        });
+
+        it('should return false if the quote itemCount is 0', () => {
+          mockQuoteStore = { data: Observable.of({ data: { itemCount: 0 } }) };
+          new QuoteEditService(mockQuoteStore, null, null).hasAssets.subscribe(d => expect(d).toBe(false));
+        });
+
+        it('should return true if the quote itemCount is greater than 0', () => {
+          mockQuoteStore = { data: Observable.of({ data: { itemCount: 1 } }) };
+          new QuoteEditService(mockQuoteStore, null, null).hasAssets.subscribe(d => expect(d).toBe(true));
+        });
+      });
+
+      describe('get quoteId', () => {
+        it('should return the quoteId', () => {
+          expect(serviceUnderTest.quoteId).toBe(3);
+        });
+      });
+
+      describe('hasProperty', () => {
+        it('should return the value of the property if the property exists', () => {
+          serviceUnderTest.hasProperty('total').subscribe(d => expect(d).toBe(90));
+        });
+
+        it('should return the undefined if the property doesnt exist', () => {
+          serviceUnderTest.hasProperty('bogusProperty').subscribe(d => expect(d).toBe(undefined));
+        });
+      });
+    });
+
+    describe('getFocusedQuote()', () => {
+      it('should call the API service correctly', () => {
+        serviceUnderTest.getFocusedQuote();
+
+        expect(mockApi.get).toHaveBeenCalledWithApi(Api.Orders);
+        expect(mockApi.get).toHaveBeenCalledWithEndpoint('quote/focused');
+        expect(mockApi.get).toHaveBeenCalledWithLoading(true);
+      });
+
+      it('should replace the quote store with the response', () => {
+        serviceUnderTest.getFocusedQuote().subscribe();
+
+        expect(mockQuoteStore.replaceQuote).toHaveBeenCalledWith(mockApi.getResponse);
+      });
+    });
+
+    describe('addProject()', () => {
+      it('calls the API service correctly', () => {
+        serviceUnderTest.addProject();
+
+        expect(mockApi.post).toHaveBeenCalledWithApi(Api.Orders);
+        expect(mockApi.post).toHaveBeenCalledWithEndpoint('quote/3/project');
+        expect(mockApi.post).toHaveBeenCalledWithLoading(true);
+      });
+
+      it('replaces the quote store with the response', () => {
+        serviceUnderTest.addProject();
+
+        expect(mockQuoteStore.replaceQuote).toHaveBeenCalledWith(mockApi.postResponse);
+      });
+    });
+
+    describe('removeProject()', () => {
+      it('should call the API service correctly', () => {
+        serviceUnderTest.removeProject({ id: '123' } as any);
+
+        expect(mockApi.delete).toHaveBeenCalledWithApi(Api.Orders);
+        expect(mockApi.delete).toHaveBeenCalledWithEndpoint('quote/3/project/123');
+        expect(mockApi.delete).toHaveBeenCalledWithLoading(true);
+      });
+
+      it('replace the quote store with the response', () => {
+        serviceUnderTest.removeProject({ id: '123' } as any);
+
+        expect(mockQuoteStore.replaceQuote).toHaveBeenCalledWith(mockApi.deleteResponse);
+      });
+    });
+
+    describe('addAssetToProjectInQuote()', () => {
+      it('should call the API service correctly', () => {
+        serviceUnderTest.addAssetToProjectInQuote({
+          lineItem: { id: '123', asset: {} }, attributes: { Distribution: 'Online Streaming' }
+        });
+
+        expect(mockApi.put).toHaveBeenCalledWithApi(Api.Orders);
+        expect(mockApi.put).toHaveBeenCalledWithEndpoint('quote/3/asset/lineItem');
+        expect(mockApi.put).toHaveBeenCalledWithBody({
+          lineItem: { id: '123', asset: {} },
+          attributes: [{ priceAttributeName: 'Distribution', selectedAttributeValue: 'Online Streaming' }]
+        });
+        expect(mockApi.put).toHaveBeenCalledWithParameters({
+          projectName: 'Project A',
+          region: 'AAA'
+        });
+      });
+
+      it('replace the quote store with the response', () => {
+        serviceUnderTest.addAssetToProjectInQuote({ id: '123' } as any);
+
+        expect(mockQuoteStore.replaceQuote).toHaveBeenCalledWith(mockApi.putResponse);
+      });
+    });
+
+    describe('updateProject()', () => {
+      it('call the API service correctly', () => {
+        serviceUnderTest.updateProject({ name: 'New Project Name' } as any);
+
+        expect(mockApi.put).toHaveBeenCalledWithApi(Api.Orders);
+        expect(mockApi.put).toHaveBeenCalledWithEndpoint('quote/3/project');
+        expect(mockApi.put).toHaveBeenCalledWithBody({ name: 'New Project Name' });
+        expect(mockApi.put).toHaveBeenCalledWithLoading(true);
+      });
+
+      it('replace the quote store with the response', () => {
+        serviceUnderTest.updateProject({ name: 'New Project Name' } as any);
+
+        expect(mockQuoteStore.replaceQuote).toHaveBeenCalledWith(mockApi.putResponse);
+      });
+    });
+
+    describe('updateProjectPriceAttributes()', () => {
+      it('should call the API service correctly', () => {
+        serviceUnderTest.updateProjectPriceAttributes(
+          { priceAttributeName: 'Distribution', selectedAttributeValue: 'Online Streaming' },
+          { id: '123', name: 'Project A', clientName: 'Ross Edfort', subtotal: 100 }
+        );
+
+        expect(mockApi.put).toHaveBeenCalledWithApi(Api.Orders);
+        expect(mockApi.put).toHaveBeenCalledWithEndpoint('quote/3/project/priceAttributes/123');
+        expect(mockApi.put).toHaveBeenCalledWithBody(
+          { priceAttributeName: 'Distribution', selectedAttributeValue: 'Online Streaming' }
+        );
+        expect(mockApi.put).toHaveBeenCalledWithLoading(true);
+      });
+
+      it('replace the quote store with the response', () => {
+        serviceUnderTest.updateProjectPriceAttributes(
+          { priceAttributeName: 'Distribution', selectedAttributeValue: 'Online Streaming' },
+          { id: '123', name: 'Project A', clientName: 'Ross Edfort', subtotal: 100 }
+        );
+
+        expect(mockQuoteStore.replaceQuote).toHaveBeenCalledWith(mockApi.putResponse);
+      });
+    });
+
+    describe('moveLineItemTo()', () => {
+      it('call the API service correctly', () => {
+        serviceUnderTest.moveLineItemTo({ id: '123' } as any, { id: '456' } as any);
+
+        expect(mockApi.put).toHaveBeenCalledWithApi(Api.Orders);
+        expect(mockApi.put).toHaveBeenCalledWithEndpoint('quote/3/move/lineItem');
+        expect(mockApi.put).toHaveBeenCalledWithParameters({ lineItemId: '456', projectId: '123' });
+        expect(mockApi.put).toHaveBeenCalledWithLoading(true);
+      });
+
+      it('replace the quote store with the response', () => {
+        serviceUnderTest.moveLineItemTo({ id: '123' } as any, { id: '456' } as any);
+
+        expect(mockQuoteStore.replaceQuote).toHaveBeenCalledWith(mockApi.putResponse);
+      });
+    });
+
+    describe('cloneLineItem()', () => {
+      it('should call the API service correctly', () => {
+        serviceUnderTest.cloneLineItem({ id: '123' });
+
+        expect(mockApi.put).toHaveBeenCalledWithApi(Api.Orders);
+        expect(mockApi.put).toHaveBeenCalledWithEndpoint('quote/3/clone/lineItem');
+        expect(mockApi.put).toHaveBeenCalledWithParameters({ lineItemId: '123' });
+        expect(mockApi.put).toHaveBeenCalledWithLoading(true);
+      });
+
+      it('replace the quote store with the response', () => {
+        serviceUnderTest.cloneLineItem({ id: '123' });
+
+        expect(mockQuoteStore.replaceQuote).toHaveBeenCalledWith(mockApi.putResponse);
+      });
+    });
+
+    describe('removeLineItem()', () => {
+      it('should call the API service correctly', () => {
+        serviceUnderTest.removeLineItem({ id: '123' });
+
+        expect(mockApi.delete).toHaveBeenCalledWithApi(Api.Orders);
+        expect(mockApi.delete).toHaveBeenCalledWithEndpoint('quote/3/asset/123');
+        expect(mockApi.delete).toHaveBeenCalledWithLoading(true);
+      });
+
+      it('should replace the store with the response', () => {
+        serviceUnderTest.removeLineItem({ id: '123' });
+
+        expect(mockQuoteStore.replaceQuote).toHaveBeenCalledWith(mockApi.deleteResponse);
+      });
+    });
+
+    describe('editLineItem()', () => {
+      it('should call the API service correctly', () => {
+        serviceUnderTest.editLineItem({ id: '123' }, { pricingAttributes: { Distribution: 'Online Streaming' } });
+
+        expect(mockApi.put).toHaveBeenCalledWithApi(Api.Orders);
+        expect(mockApi.put).toHaveBeenCalledWithEndpoint('quote/3/update/lineItem/123');
+        expect(mockApi.put).toHaveBeenCalledWithBody({
+          id: '123', attributes: [{ priceAttributeName: 'Distribution', selectedAttributeValue: 'Online Streaming' }]
+        });
+        expect(mockApi.put).toHaveBeenCalledWithParameters({ region: 'AAA' });
+      });
+
+      it('should replace the quote store with the response', () => {
+        serviceUnderTest.editLineItem({ id: '123' }, { pricingAttributes: { Distribution: 'Online Streaming' } });
+
+        expect(mockQuoteStore.replaceQuote).toHaveBeenCalledWith(mockApi.putResponse);
+      });
+    });
+
+    describe('updateQuoteField()', () => {
+      it('should call the API service correctly - add', () => {
+        serviceUnderTest.updateQuoteField({ bulkOrderId: 'abc-123' });
+
+        const expectedBody = Object.assign(
+          { id: 3, ownerUserId: 10, total: 90, subTotal: 100, projects: [{ name: 'Project A' }] },
+          { bulkOrderId: 'abc-123' }
+        );
+
+        expect(mockApi.put).toHaveBeenCalledWithApi(Api.Orders);
+        expect(mockApi.put).toHaveBeenCalledWithEndpoint('quote/3');
+        expect(mockApi.put).toHaveBeenCalledWithBody(expectedBody);
+      });
+
+      it('should call the API service correctly - remove', () => {
+        mockQuoteStore.state = {
+          data: { id: 3, ownerUserId: 10, total: 90, subTotal: 100, bulkOrderId: 'abc-123', projects: [{ name: 'Project A' }] }
+        };
+
+        serviceUnderTest.updateQuoteField({ bulkOrderId: '' });
+
+        expect(mockApi.put).toHaveBeenCalledWithApi(Api.Orders);
+        expect(mockApi.put).toHaveBeenCalledWithEndpoint('quote/3');
+        expect(mockApi.put).toHaveBeenCalledWithBody({
+          id: 3, ownerUserId: 10, total: 90, subTotal: 100, projects: [{ name: 'Project A' }]
+        });
+      });
+
+      it('should replace the quote store with the response', () => {
+        serviceUnderTest.updateQuoteField({ bulkOrderId: 'abc-123' });
+
+        expect(mockQuoteStore.replaceQuote).toHaveBeenCalledWith(mockApi.putResponse);
+      });
     });
 
     describe('sendQuote', () => {
