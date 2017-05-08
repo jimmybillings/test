513c818af625c562bc36fda2cf517b9bb4189ab6
Ed Tramell <ed.tramell@wazeedigital.com>
2017-05-08 16:59:39 -0600

    CRUX-2004: Remove logic from AssetSubclipDisplayComponent's template

--- src/client/app/+commerce/components/asset/asset-subclip-display.component.spec.ts
+++ src/client/app/+commerce/components/asset/asset-subclip-display.component.spec.ts
@@ -3,9 +3,10 @@ import { AssetSubclipDisplayComponent } from './asset-subclip-display.component'
 export function main() {
   describe('Asset Subclip Display Component', () => {
     let componentUnderTest: AssetSubclipDisplayComponent;
+    let mockAssetService: any;
 
     beforeEach(() => {
-      componentUnderTest = new AssetSubclipDisplayComponent();
+      componentUnderTest = new AssetSubclipDisplayComponent(mockAssetService);
     });
 
     it('has no tests!', () => {
--- src/client/app/+commerce/components/asset/asset-subclip-display.component.ts
+++ src/client/app/+commerce/components/asset/asset-subclip-display.component.ts
@@ -1,45 +1,71 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+
 import { Frame, TimecodeFormat } from 'wazee-frame-formatter';
+import { Asset } from '../../../shared/interfaces/commerce.interface';
+import { EnhancedAsset } from '../../../shared/interfaces/enhanced-asset';
+import { AssetService } from '../../../shared/services/asset.service';
 
 @Component({
   moduleId: module.id,
   selector: 'asset-subclip-display-component',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div *ngIf="asset.timeStart !== -2" class="asset-sub-clip md-caption">
-      <strong>{{ 'COLLECTION.SHOW.SUB_CLIPPING_LABEL' | translate }} </strong>
+    <div *ngIf="isSubclipped" class="asset-sub-clip md-caption">
+      <strong>{{ 'COLLECTION.SHOW.SUB_CLIPPING_LABEL' | translate }}</strong>
       <div class="subclip-timeline">
-        <span class="subclip-segment" [ngStyle]="calcSubClipSegments(asset)"></span>
+        <span class="subclip-segment" [ngStyle]="subclipSegmentStyles"></span>
       </div>
       <ul class="subclip-data" layout="row" hide-sm>
         <li flex-gt-sm="30" flex-gt-md="25" flex="35">
-          <strong>{{ 'COLLECTION.SHOW.START_TIME_LABEL' | translate }} </strong>
-          {{frame(asset.metadata[2].value, asset.timeStart - 0) | timecode}}
+          <strong>{{ 'COLLECTION.SHOW.START_TIME_LABEL' | translate }}</strong>
+          {{ inMarkerFrame | timecode }}
         </li>
         <li flex="none">
           <strong>{{ 'COLLECTION.SHOW.END_TIME_LABEL' | translate }} </strong>
-          {{frame(asset.metadata[2].value, asset.timeEnd - 0) | timecode}}
+          {{ outMarkerFrame | timecode }}
         </li>
         <li flex="" layout-align="end center">
-          <strong>{{ 'COLLECTION.SHOW.SUB_CLIP_LENGTH_LABEL' | translate }} </strong>
-          {{frame(asset.metadata[2].value, asset.timeEnd - asset.timeStart) | timecode}}
+          <strong>{{ 'COLLECTION.SHOW.SUB_CLIP_LENGTH_LABEL' | translate }}</strong>
+          {{ subclipDurationFrame | timecode }}
         </li>
       </ul>
     </div>
   `
 })
 export class AssetSubclipDisplayComponent {
-  @Input() asset: any;
+  private enhancedAsset: EnhancedAsset;
+
+  constructor(private assetService: AssetService) { }
+
+  @Input() public set asset(asset: Asset) {
+    this.enhancedAsset = this.assetService.enhance(asset);
+  }
+
+  public get isSubclipped(): boolean {
+    return this.enhancedAsset.timeStart !== -2;
+  }
+
+  public get inMarkerFrame(): Frame {
+    return this.frame(this.enhancedAsset.metadata[2].value, this.enhancedAsset.timeStart - 0);
+  }
+
+  public get outMarkerFrame(): Frame {
+    return this.frame(this.enhancedAsset.metadata[2].value, this.enhancedAsset.timeEnd - 0);
+  }
+
+  public get subclipDurationFrame(): Frame {
+    return this.frame(this.enhancedAsset.metadata[2].value, this.enhancedAsset.timeEnd - this.enhancedAsset.timeStart);
+  }
 
   public frame(framesPerSecond: any, frameNumber: any) {
     return new Frame(framesPerSecond.split(' fps')[0]).setFromFrameNumber(frameNumber);
   }
 
-  public calcSubClipSegments(asset: any) {
-    let lengthAsSeconds: number = asset.metadata[5].value / 1000.0;
-    let totalFrames: number = this.durationAsFrames(asset.metadata[2].value, lengthAsSeconds);
-    let startPoint: number = this.calcSegmentWidthPecentage(asset.timeStart, totalFrames);
-    let segmentWidth: number = this.calcSegmentWidthPecentage(asset.timeEnd - asset.timeStart, totalFrames);
+  public get subclipSegmentStyles(): object {
+    let lengthAsSeconds: number = parseInt(this.enhancedAsset.metadata[5].value) / 1000.0;
+    let totalFrames: number = this.durationAsFrames(this.enhancedAsset.metadata[2].value, lengthAsSeconds);
+    let startPoint: number = this.calcSegmentWidthPecentage(this.enhancedAsset.timeStart, totalFrames);
+    let segmentWidth: number = this.calcSegmentWidthPecentage(this.enhancedAsset.timeEnd - this.enhancedAsset.timeStart, totalFrames);
     return { 'width.%': segmentWidth, 'margin-left.%': startPoint, 'min-width.px': 2 };
   }
 
