e011efbf2efdfe8eb568bd50af48bce74b8bb2a3
Ross Edfort <ross.edfort@wazeedigital.com>
2017-05-16 10:18:41 -0600

    CRUX-2044 add cost multiplier display to line items

--- src/client/app/+commerce/components/line-item/line-item-price.component.ts
+++ src/client/app/+commerce/components/line-item/line-item-price.component.ts
@@ -9,14 +9,23 @@ import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
     <div class="price" [ngClass]="{'select-usage': needsAttributes }">
       {{ price | currency:'USD':true:'1.2-2' }}
     </div>
+    <div class="multiplier" *ngIf="shouldShowMultiplier">
+      <span>{{ 'QUOTE.MULTIPLIER_INDICATOR' | translate:{multiplier: multiplier} }}</span>
+    </div>
   `
 })
 export class LineItemPriceComponent {
   @Input() price: number;
+  @Input() multiplier: number;
+  @Input() userCanAdministerQuotes: boolean;
   @Input() rightsManaged: string;
   @Input() hasAttributes: boolean;
 
   public get needsAttributes(): boolean {
     return this.rightsManaged === 'Rights Managed' && !this.hasAttributes;
+  }
+
+  public get shouldShowMultiplier(): boolean {
+    return this.userCanAdministerQuotes && this.multiplier > 1;
   }
 }
