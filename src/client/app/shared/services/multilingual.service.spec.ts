8b8be17b9c48328d596eceb919eb5d23d79ad997
Ross Edfort <ross.edfort@wazeedigital.com>
2017-05-25 16:26:20 -0600

    CRUX-1892 - fix MultilingualService spec

--- src/client/app/shared/services/multilingual.service.spec.ts
+++ src/client/app/shared/services/multilingual.service.spec.ts
@@ -1,70 +1,40 @@
-import {
-  beforeEachProvidersArray,
-  TestBed,
-  inject
-} from '../../imports/test.imports';
-
 import { MultilingualService } from './multilingual.service';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { ApiConfig } from './api.config';
-
-import { Http } from '@angular/http';
-
-export function createTranslateLoader(http: Http) {
-  return new TranslateHttpLoader(http, 'assets/i18n', '.json');
-}
-
-class MockApiConfig {
-  public get portal(): string {
-    return 'core';
-  }
-  public get baseUrl(): string {
-    return 'http://crxextapi.dev.wzplatform.com/';
-  }
-}
+import { Observable } from 'rxjs/Observable';
 
 export function main() {
+  let serviceUnderTest: MultilingualService, mockApiConfig: any, mockStore: any, mockTranslate: any,
+    mockState: any;
+
+  beforeEach(() => {
+    mockTranslate = { use: jasmine.createSpy('use') };
+    mockApiConfig = { baseUrl: 'http://crxextapi.dev.wzplatform.com/', portal: 'core' };
+    mockState = { code: 'en', title: 'English' };
+    mockStore = {
+      dispatch: jasmine.createSpy('dispatch'),
+      select: jasmine.createSpy('select').and.returnValue(Observable.of(mockState))
+    };
+    serviceUnderTest = new MultilingualService(mockTranslate, mockApiConfig, mockStore);
+  });
+
   describe('Multilingual Service', () => {
-
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        imports: [
-          TranslateModule.forRoot({
-            loader: {
-              provide: TranslateLoader,
-              useFactory: createTranslateLoader,
-              deps: [Http]
-            }
-          })
-        ],
-        providers: [
-          ...beforeEachProvidersArray,
-          MultilingualService,
-          { provide: ApiConfig, useClass: MockApiConfig },
-        ]
+    describe('constructor', () => {
+      it('Should set the default language to English', () => {
+        expect(mockStore.dispatch).toHaveBeenCalledWith({
+          type: '[Multilingual] LANG_CHANGE',
+          payload: { lang: '/crxextapi.dev.wzplatform.com/identities-api/v1/translation/core/en' }
+        });
       });
     });
 
-    it('should at a minimum support english', () => {
-      expect(MultilingualService.SUPPORTED_LANGUAGES.length).toBeGreaterThan(0);
-      expect(MultilingualService.SUPPORTED_LANGUAGES[0].code).toBe('en');
+    describe('setLanguage', () => {
+      it('Should change the current language', () => {
+        serviceUnderTest.setLanguage('fr');
+
+        expect(serviceUnderTest.store.dispatch).toHaveBeenCalledWith({
+          type: '[Multilingual] LANG_CHANGE',
+          payload: { lang: '/crxextapi.dev.wzplatform.com/identities-api/v1/translation/core/fr' }
+        });
+      });
     });
-
-    it('Should set the default language to English', inject([MultilingualService], (service: MultilingualService) => {
-      service.store.select('i18n').subscribe((i18n: any) => {
-        expect(i18n.lang).toBe('/crxextapi.dev.wzplatform.com/identities-api/v1/translation/core/en');
-      });
-    }));
-
-    it('Should change the current language', inject([MultilingualService], (service: MultilingualService) => {
-      spyOn(service.store, 'dispatch').and.callThrough();
-      service.setLanguage('fr');
-      expect(service.store.dispatch).toHaveBeenCalledWith(
-        { type: '[Multilingual] LANG_CHANGE', payload: { lang: '/crxextapi.dev.wzplatform.com/identities-api/v1/translation/core/fr' } });
-      service.store.select('i18n').subscribe((i18n: any) => {
-        expect(i18n.lang).toBe('/crxextapi.dev.wzplatform.com/identities-api/v1/translation/core/fr');
-      });
-    }));
   });
 }
