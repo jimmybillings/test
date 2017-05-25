5186fabb3b9cf8dcecf6b3a35b367f0695074d14
Ross Edfort <ross.edfort@wazeedigital.com>
2017-05-25 13:18:11 -0600

    CRUX-1885 - fix CollectionFormComponent spec

--- src/client/app/application/collection-tray/components/collection-form.component.spec.ts
+++ src/client/app/application/collection-tray/components/collection-form.component.spec.ts
@@ -1,90 +1,32 @@
-import {
-  beforeEachProvidersArray,
-  Observable,
-  Injectable,
-  inject,
-  TestBed
-} from '../../../imports/test.imports';
-
 import { CollectionFormComponent } from './collection-form.component';
-import { CollectionsService } from '../../../shared/services/collections.service';
-import { ActiveCollectionService } from '../../../shared/services/active-collection.service';
+import { Observable } from 'rxjs/Observable';
 
 export function main() {
-  describe('Collection Form component', () => {
-    @Injectable()
-    class MockCollectionsService {
-      public data: Observable<any>;
-      constructor() {
-        this.data = Observable.of(mockCollections());
-      }
-      public create(collection: any): Observable<any> {
-        return Observable.of(mockCollection());
-      }
-      public createCollectionInStore(collection: any): any {
-        return true;
-      }
-      public updateFocusedCollection(collection: any): any {
-        return true;
-      }
-    }
+  let componentUnderTest: CollectionFormComponent, mockCollections: any, mockCollectionContext: any, mockActiveCollection: any;
 
-    class MockActiveCollectionService {
-      public data: Observable<any>;
-      constructor() {
-        this.data = Observable.of({ id: 1 });
-      }
-      load() {
-        return Observable.of({ id: 2 });
-      }
-      set() {
-        return Observable.of({});
-      }
+  describe('Collection Form Component', () => {
+    beforeEach(() => {
+      mockCollections = {
+        create: jasmine.createSpy('create').and.returnValue(Observable.of({})),
+        load: jasmine.createSpy('load').and.returnValue(Observable.of({}))
+      };
+      mockActiveCollection = { load: jasmine.createSpy('load').and.returnValue(Observable.of({})) };
+      mockCollectionContext = { resetCollectionOptions: jasmine.createSpy('resetCollectionOptions') };
+      componentUnderTest = new CollectionFormComponent(mockCollections, mockActiveCollection, null, null, mockCollectionContext);
+      componentUnderTest.dialog = { close: () => { } };
+    });
 
-      getItems() {
-        return Observable.of({});
-      }
-    }
-
-    beforeEach(() => TestBed.configureTestingModule({
-      providers: [
-        ...beforeEachProvidersArray,
-        CollectionFormComponent,
-        { provide: CollectionsService, useClass: MockCollectionsService },
-        { provide: ActiveCollectionService, useClass: MockActiveCollectionService }
-      ]
-    }));
-
-    it('Should create a new collection',
-      inject([CollectionFormComponent], (component: CollectionFormComponent) => {
-        component.dialog = {};
-        component.dialog.close = function () { return true; };
-        spyOn(component.collections, 'create').and.callThrough();
-        spyOn(component.activeCollection, 'load').and.callThrough();
-        spyOn(component, 'loadCollections');
-        component.createCollection(mockCollection());
+    describe('createCollection()', () => {
+      it('Should create a new collection', () => {
+        spyOn(componentUnderTest, 'loadCollections');
+        componentUnderTest.createCollection(mockCollection());
         let collectionWithParsedTags = mockCollection();
         collectionWithParsedTags.tags = ['cat', 'dog', 'cow'];
-        expect(component.loadCollections).toHaveBeenCalled();
-        expect(component.collections.create).toHaveBeenCalledWith(collectionWithParsedTags);
-        expect(component.activeCollection.load).toHaveBeenCalled();
-      }));
-
-    // it('Should return type ahead suggestions matching input',
-    //   inject([CollectionFormComponent], (component: CollectionFormComponent) => {
-    //     expect(component.getSuggestions('maui', mockCollections()))
-    //       .toEqual(['Maui Hawaii', 'Maui Hawaii +25', 'Maui Hawaii five-o', 'Maui Hawaii five-99', 'Maui Hawaii testing']);
-    //   }));
-
-    // it('Should be able to use down arrow key to navigate type ahead suggestion list',
-    //   inject([CollectionFormComponent], (component: CollectionFormComponent) => {
-    //     spyOn(component, 'inputKeyDown').and.callThrough();
-    //     spyOn(component, 'setActiveSuggestion');
-    //     spyOn(component, 'getActiveSuggestionIndex');
-    //     component.inputKeyDown(mockKeyboardEventDownArrow());
-    //     expect(component.getActiveSuggestionIndex).toHaveBeenCalled();
-    //     expect(component.setActiveSuggestion).toHaveBeenCalled();
-    //   }));
+        expect(componentUnderTest.loadCollections).toHaveBeenCalled();
+        expect(componentUnderTest.collections.create).toHaveBeenCalledWith(collectionWithParsedTags);
+        expect(componentUnderTest.activeCollection.load).toHaveBeenCalled();
+      });
+    });
   });
 }
 
@@ -100,19 +42,4 @@ function mockCollection(): any {
   };
 }
 
-function mockCollections(): any {
-  return {
-    'items': [
-      {
-        'id': 196,
-        'name': 'Taxi Cabs', 'owner': 62,
-        'email': 'jeff+2@jeffhyde.com',
-        'userRole': 'owner',
-        'tags': ['cabs', 'taxi', 'france'],
-        'assetsCount': 24,
-        'createdOn': '2016-06-21T18:15:28Z',
-        'lastUpdated': '2016-07-15T00:07:08Z',
-      }]
-  };
-}
 
