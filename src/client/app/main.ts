aaba0ea34442d241f0d07bd1cc4e1d7672e496a9
James Billings <james_billings@me.com>
2017-06-09 15:27:36 -0600

    INTERNAL - build library

--- src/client/app/main.ts
+++ src/client/app/main.ts
@@ -1,4 +1,4 @@
-require('hammerjs');
+import 'hammerjs';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
