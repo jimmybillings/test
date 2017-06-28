c66065a8b57f8b8b8ed42a9c7077844f256a57ec
Ross Edfort <ross.edfort@wazeedigital.com>
2017-06-28 10:32:06 -0600

    CRUX-1981 - add cart tab component specs

--- src/client/app/+commerce/+cart/components/tabs/cart-tab.component.spec.ts
+++ src/client/app/+commerce/+cart/components/tabs/cart-tab.component.spec.ts
@@ -1,15 +1,59 @@
 import { CartTabComponent } from './cart-tab.component';
+import { Observable } from 'rxjs/Observable';
 
 export function main() {
   describe('Cart Tab Component', () => {
-    let componentUnderTest: CartTabComponent;
+    let componentUnderTest: CartTabComponent, mockUserCan: any, mockCartService: any, mockDialogService: any;
 
     beforeEach(() => {
-      componentUnderTest = new CartTabComponent(null, null, null, null, null, null, null, null, null, null, null, null, null);
+      mockUserCan = {
+        viewLicenseAgreementsButton: jasmine.createSpy('viewLicenseAgreementsButton')
+      };
+
+      mockCartService = {
+        getPaymentOptions: jasmine.createSpy('createPaymentOptions'),
+        retrieveLicenseAgreements: jasmine.createSpy('retrieveLicenseAgreements').and.returnValue(Observable.of({}))
+      };
+
+      mockDialogService = {
+        openComponentInDialog: jasmine.createSpy('openComponentInDialog')
+      };
+
+      componentUnderTest = new CartTabComponent(
+        mockUserCan, mockCartService, null, mockDialogService, null,
+        null, null, null, null, null, null, null, null
+      );
     });
 
-    it('has no tests!', () => {
-      expect(true).toBe(true);
+    describe('checkout()', () => {
+      it('should go to the next tab', () => {
+        spyOn(componentUnderTest, 'goToNextTab');
+        componentUnderTest.checkout();
+
+        expect(componentUnderTest.goToNextTab).toHaveBeenCalled();
+      });
+
+      it('should retrieve the payment options', () => {
+        componentUnderTest.checkout();
+
+        expect(mockCartService.getPaymentOptions).toHaveBeenCalled();
+      });
+    });
+
+    describe('shouldShowLicenseDetailsBtn()', () => {
+      it('should cal viewLicenseAgreementsButton on the commerce capabilities', () => {
+        componentUnderTest.shouldShowLicenseDetailsBtn();
+
+        expect(mockUserCan.viewLicenseAgreementsButton).toHaveBeenCalled();
+      });
+    });
+
+    describe('showLicenseAgreements()', () => {
+      it('should call retrieveLicenseAgreements() on the cart service', () => {
+        componentUnderTest.showLicenseAgreements();
+
+        expect(mockCartService.retrieveLicenseAgreements).toHaveBeenCalled();
+      });
     });
   });
 }
