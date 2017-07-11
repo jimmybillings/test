5093541f1d87a6284f0fe64090ad6e8ca5ed2df6
James Billings <james_billings@me.com>
2017-07-11 16:10:02 -0600

    INTERNAL - add gzip

--- build.sh
+++ build.sh
@@ -57,9 +57,6 @@ trap clean_up EXIT
 build_prod() {
   npm run build.prod.aot || exit 1
 
-  # Upload to S3
-  aws s3 cp "${baseDir}/dist/prod" s3://dmhint01.dv.wzplatform.com/ --profile tem-dv-svc-aws-upload-1 --recursive
-
   # create build.properties file
   set-maven-build-information.sh --path=${baseDir}/dist/prod --version=${buildVersion}
   create-status-html.sh "${baseDir}/dist/prod/build.properties" "${baseDir}/dist/prod/status.html"
@@ -84,6 +81,12 @@ build_prod() {
     # put the calculated artifact version into a properties file so jenkins can find it
     create-jenkins-properties.sh ${buildVersion}
   fi
+
+  cd "${baseDir}/dist/prod"
+  find . -type f -exec gzip "{}" \; -exec mv "{}.gz" "{}" \;
+  # Upload to S3
+  cd -
+  aws s3 cp "${baseDir}/dist/prod" s3://dmhint01.dv.wzplatform.com/ --profile tem-dv-svc-aws-upload-1 --recursive modify --add-header="Content-Encoding:gzip"
   return 0
 }
 
@@ -142,7 +145,7 @@ npm install
 build_prod
 
 # build the UI library
-build_library
+# build_library
 
 cd "$TMPDIR/wazee-crux-version-control"
 git pull origin develop
