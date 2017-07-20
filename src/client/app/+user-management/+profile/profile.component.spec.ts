d7b506fef2b87f56e38b15a90d74d059f06f139a
jhyde <jeff@jeffhyde.com>
2017-07-20 14:44:52 -0600

    INTERNAL - change test assumption text

--- src/client/app/+user-management/+profile/profile.component.spec.ts
+++ src/client/app/+user-management/+profile/profile.component.spec.ts
@@ -48,7 +48,7 @@ export function main() {
         let result = componentUnderTest.getBillingAddressInfo('state');
         expect(result).toBe('');
       });
-      it('should return undefined when part of billingInfo exist but requested part is missing', () => {
+      it('should return undefined when part of billingInfo.address exist but requested part is missing', () => {
         mockUser = {
           emailAddress: 'jdoe@gmail.com',
           firstName: 'John', lastName: 'Doe', password: '3978f324e14ac256b2994b754586e05f',
