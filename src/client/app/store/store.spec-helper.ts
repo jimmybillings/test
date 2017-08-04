460a0cf6f588096c11d777290807b1130dcd15dd
Ed Tramell <ed.tramell@wazeedigital.com>
2017-08-04 17:31:21 -0600

    CRUX-2014: Improve standard action test failure output

--- src/client/app/store/store.spec-helper.ts
+++ src/client/app/store/store.spec-helper.ts
@@ -241,10 +241,22 @@ export class StoreSpecHelper {
 
     describe(`${methodName}()`, () => {
       it(`creates the expected action${optionalComment}`, () => {
-        const createdAction =
-          (new parameters.factoryMethod.class() as any)[methodName](...parameters.factoryMethod.parameters);
+        const factoryMethod = (new parameters.factoryMethod.class() as any)[methodName];
 
-        expect(Object.keys(createdAction).length).toBe(Object.keys(parameters.expectedAction).length);
+        if (!factoryMethod) {
+          fail(`Could not find a method named '${methodName}' in the specified action factory.`);
+        }
+
+        const createdAction: any = factoryMethod(...parameters.factoryMethod.parameters);
+        const createdActionParameterCount: number = Object.keys(createdAction).length - 1; // exclude 'type'
+        const expectedActionParameterCount: number = Object.keys(parameters.expectedAction).length - 1; // exclude 'type'
+
+        if (createdActionParameterCount !== expectedActionParameterCount) {
+          fail(`Expected created action to have`
+            + ` ${expectedActionParameterCount} parameter${expectedActionParameterCount === 1 ? '' : 's'},`
+            + ` but it had ${createdActionParameterCount}.`
+          );
+        }
 
         Object.keys(createdAction).forEach(key => {
           expect(createdAction[key]).toEqual(parameters.expectedAction[key]);
