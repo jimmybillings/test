77871653fc3ed8f4faa2a2812271dd518294a77d
Ed Tramell <ed.tramell@wazeedigital.com>
2017-08-04 22:22:25 -0600

    CRUX-2014: Use standard mockStore in CollectionFormComponent spec

--- src/client/app/application/collection-tray/components/collection-form.component.spec.ts
+++ src/client/app/application/collection-tray/components/collection-form.component.spec.ts
@@ -1,9 +1,11 @@
-import { CollectionFormComponent } from './collection-form.component';
 import { Observable } from 'rxjs/Observable';
+
+import { CollectionFormComponent } from './collection-form.component';
+import { StoreSpecHelper } from '../../../store/store.spec-helper';
 
 export function main() {
   let componentUnderTest: CollectionFormComponent, mockCollections: any, mockCollectionContext: any, mockActiveCollection: any;
-  let mockStore: any;
+  let storeSpecHelper: StoreSpecHelper;
 
   describe('Collection Form Component', () => {
     beforeEach(() => {
@@ -12,21 +14,22 @@ export function main() {
         load: jasmine.createSpy('load').and.returnValue(Observable.of({}))
       };
       mockCollectionContext = { resetCollectionOptions: jasmine.createSpy('resetCollectionOptions') };
-      mockStore = { dispatch: jasmine.createSpy('dispatch') };
+      storeSpecHelper = new StoreSpecHelper();
       componentUnderTest =
-        new CollectionFormComponent(mockCollections, null, null, mockCollectionContext, mockStore);
+        new CollectionFormComponent(mockCollections, null, null, mockCollectionContext, storeSpecHelper.mockStore);
       componentUnderTest.dialog = { close: () => { } };
     });
 
     describe('createCollection()', () => {
       it('Should create a new collection', () => {
+        const spy = storeSpecHelper.createMockActionFactoryMethod(factory => factory.activeCollection, 'load');
         spyOn(componentUnderTest, 'loadCollections');
         componentUnderTest.createCollection(mockCollection());
         let collectionWithParsedTags = mockCollection();
         collectionWithParsedTags.tags = ['cat', 'dog', 'cow'];
         expect(componentUnderTest.loadCollections).toHaveBeenCalled();
         expect(componentUnderTest.collections.create).toHaveBeenCalledWith(collectionWithParsedTags);
-        // expect(mockStore.dispatch).toHaveBeenCalledWith(new ActiveCollectionActions.Load());
+        storeSpecHelper.expectDispatchFor(spy);
       });
     });
   });
