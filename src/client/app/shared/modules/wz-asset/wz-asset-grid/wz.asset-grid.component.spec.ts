26d8cf81cbe826b42c2681d9070a6d36cf5430e1
David Davydov <kctrlv@gmail.com>
2017-08-09 11:43:06 -0600

    CRUX-1339 add spec for assetCanBePurchased

--- src/client/app/shared/modules/wz-asset/wz-asset-grid/wz.asset-grid.component.spec.ts
+++ src/client/app/shared/modules/wz-asset/wz-asset-grid/wz.asset-grid.component.spec.ts
@@ -1,15 +1,49 @@
 import { WzAssetGridComponent } from './wz.asset-grid.component';
 
 export function main() {
-  xdescribe('Wz Asset Grid Component', () => {
+  describe('Wz Asset Grid Component', () => {
     let componentUnderTest: WzAssetGridComponent;
 
     beforeEach(() => {
       componentUnderTest = new WzAssetGridComponent(null, null);
     });
 
-    xit('has no tests!', () => {
-      expect(true).toBe(true);
+    describe('assetCanBePurchased(asset)', () => {
+      it('is false when asset is missing a Rights.Reproduction metadata field', () => {
+        let asset = {
+          metaData: [
+            { name: 'someKey', value: 'someValue' }
+          ]
+        };
+        expect(componentUnderTest.assetCanBePurchased(asset)).toBe(false);
+      });
+
+      it('is false when asset has an unaccepted value in the Rights.Reproduction metadata field', () => {
+        let asset = {
+          metaData: [
+            { name: 'Rights.Reproduction', value: 'someValue2' }
+          ]
+        };
+        expect(componentUnderTest.assetCanBePurchased(asset)).toBe(false);
+      });
+
+      it('is true when Rights.Reproduction is Royalty Free', () => {
+        let asset = {
+          metaData: [
+            { name: 'Rights.Reproduction', value: 'Royalty Free' }
+          ]
+        };
+        expect(componentUnderTest.assetCanBePurchased(asset)).toBe(true);
+      });
+
+      it('is true when Rights.Reproduction is Rights Managed', () => {
+        let asset = {
+          metaData: [
+            { name: 'Rights.Reproduction', value: 'Rights Managed' }
+          ]
+        };
+        expect(componentUnderTest.assetCanBePurchased(asset)).toBe(true);
+      });
     });
   });
 }
