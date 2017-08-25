d6590109b2d7a573d523070f27feb8b6afdad7f9
Ross Edfort <ross.edfort@wazeedigital.com>
2017-08-25 16:07:19 -0600

    CRUX-2518 - add spec for collection asset load

--- src/client/app/store/effects/asset.effects.spec.ts
+++ src/client/app/store/effects/asset.effects.spec.ts
@@ -38,5 +38,35 @@ export function main() {
         });
       });
     });
+
+    describe('loadCollectionAsset', () => {
+      it('works as expected', () => {
+        effectsSpecHelper.generateStandardTestFor({
+          effectName: 'loadCollectionAsset',
+          effectsInstantiator: instantiator,
+          state: {
+            storeSectionName: 'activeCollection',
+            propertyName: 'collection',
+            value: { assets: { items: [{ uuid: 'abc-123', assetId: 456, timeStart: 100, timeEnd: 1000 }] } }
+          },
+          inputAction: {
+            type: AssetActions.LoadCollectionAsset.Type,
+            loadParameters: { uuid: 'abc-123' }
+          },
+          serviceMethod: {
+            name: 'load',
+            expectedArguments: [
+              { uuid: 'abc-123', id: '456', timeStart: '100', timeEnd: '1000' }
+            ],
+            returnsObservableOf: { some: 'asset' }
+          },
+          outputActionFactory: {
+            sectionName: 'asset',
+            methodName: 'loadSuccess',
+            expectedArguments: [{ some: 'asset' }]
+          }
+        });
+      });
+    });
   });
 }
