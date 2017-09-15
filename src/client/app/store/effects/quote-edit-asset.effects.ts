ce240d60a2c9a8631e842df69929e2e6b9f49fa5
Ed Tramell <ed.tramell@wazeedigital.com>
2017-09-14 18:22:19 -0600

    INTERNAL: Fix lost quote edit asset UUID bug

--- src/client/app/store/effects/quote-edit-asset.effects.ts
+++ src/client/app/store/effects/quote-edit-asset.effects.ts
@@ -53,7 +53,7 @@ export class QuoteEditAssetEffects {
 
     return {
       id: String(asset.assetId),
-      uuid: String(asset.uuid),
+      uuid: assetUuid,
       timeStart: String(asset.timeStart),
       timeEnd: String(asset.timeEnd)
     };
