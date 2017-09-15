d479156c384f9344e294d7acf186cb2e8f5db9d3
jhyde <jeff@jeffhyde.com>
2017-09-15 16:37:11 -0600

    CRUX-2602 created spec - no tests needed

---
+++ src/client/app/+commerce/components/line-item/line-item-rights.component.spec.ts
@@ -0,0 +1,15 @@
+import { LineItemRightsComponent } from './line-item-rights.component';
+
+export function main() {
+  describe('Line Item Rights Component', () => {
+    let componentUnderTest: LineItemRightsComponent;
+
+    beforeEach(() => {
+      componentUnderTest = new LineItemRightsComponent();
+    });
+
+    it('***** HASN\'T BEEN TESTED YET! *****', () => {
+      expect(true).toBe(true);
+    });
+  });
+}
