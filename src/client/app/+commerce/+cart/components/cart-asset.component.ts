c470b67e696ff585dea61fba542c9b9d266e3ee7
Ed Tramell <ed.tramell@wazeedigital.com>
2017-09-21 15:30:28 -0600

    CRUX-2329, CRUX-2331, CRUX-2591: Add cart lineItem comments

--- src/client/app/+commerce/+cart/components/cart-asset.component.ts
+++ src/client/app/+commerce/+cart/components/cart-asset.component.ts
@@ -1,14 +1,32 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 
 import { StateMapper } from '../../../app.store';
 import { Asset } from '../../../shared/interfaces/common.interface';
+import { FormFields } from '../../../shared/interfaces/forms.interface';
+import { UiConfig } from '../../../shared/services/ui.config';
 
 @Component({
   moduleId: module.id,
   selector: 'cart-asset',
-  template: `<asset-component [stateMapper]="stateMapper" [assetType]="'cartAsset'"></asset-component>`,
+  template: `
+    <asset-component
+      [stateMapper]="stateMapper"
+      [assetType]="'cartAsset'"
+      [commentFormConfig]="commentFormConfig">
+    </asset-component>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CartAssetComponent {
+export class CartAssetComponent implements OnInit {
+  public commentFormConfig: FormFields;
+
+  constructor(private uiConfig: UiConfig) { }
+
+  public ngOnInit(): void {
+    this.uiConfig.get('cartComment').take(1).subscribe((config: any) => {
+      this.commentFormConfig = config.config.form.items;
+    });
+  }
+
   public stateMapper: StateMapper<Asset> = (state) => state.cartAsset.activeAsset;
 }
