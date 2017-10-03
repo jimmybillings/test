f783019e254781f82247db47162ae4f84b962208
David Davydov <kctrlv@gmail.com>
2017-10-03 14:01:45 -0600

    CRUX-2614 add asset-share component spec

--- src/client/app/+asset/components/asset-share.component.spec.ts
+++ src/client/app/+asset/components/asset-share.component.spec.ts
@@ -1,22 +1,19 @@
-import { Observable } from 'rxjs/Observable';
-
 import { AssetShareComponent } from './asset-share.component';
 
 export function main() {
   describe('Asset Share Component', () => {
     let componentUnderTest: AssetShareComponent;
-    let mockCurrentUserService: any;
 
     beforeEach(() => {
-      // TODO: This is a minimal mock that exists solely to stop
-      // the constructor from failing.  Enhance as needed.
-      mockCurrentUserService = { data: Observable.of({}) };
-
       componentUnderTest = new AssetShareComponent(null, null);
     });
 
-    it('***** HASN\'T BEEN TESTED YET! *****', () => {
-      expect(true).toBe(true);
+    describe('ngOnDestroy', () => {
+      it('calls the closeAssetShare method', () => {
+        spyOn(componentUnderTest, 'closeAssetShare').and.callThrough();
+        componentUnderTest.ngOnDestroy();
+        expect(componentUnderTest.closeAssetShare).toHaveBeenCalled();
+      });
     });
   });
 };
