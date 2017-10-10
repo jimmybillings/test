8274d136770badced23ad5f5a22174fb63ee9f63
James Billings <james_billings@me.com>
2017-10-10 15:50:49 -0600

    INTERNAL - updates

--- src/client/app/shared/modules/wz-design/wz.design.module.ts
+++ src/client/app/shared/modules/wz-design/wz.design.module.ts
@@ -1,59 +1,38 @@
-import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 
-// import {
-//   // MatAutocompleteModule,
-//   // MatButtonModule,
-//   MatButtonToggleModule,
-//   MatCardModule,
-//   MatCheckboxModule,
-//   // MatChipsModule,
-//   // MatDatepickerModule,
-//   MatDialogModule,
-//   // MatExpansionModule,
-//   MatFormFieldModule,
-//   MatGridListModule,
-//   MatIconModule,
-//   MatInputModule,
-//   MatListModule,
-//   MatMenuModule,
-//   MatPaginatorModule,
-//   MatProgressBarModule,
-//   // MatProgressSpinnerModule,
-//   MatRadioModule,
-//   MatSelectModule,
-//   MatSidenavModule,
-//   MatSliderModule,
-//   MatSlideToggleModule,
-//   MatSnackBarModule,
-//   // MatSortModule,
-//   // MatTableModule,
-//   MatTabsModule,
-//   MatToolbarModule,
-//   MatTooltipModule,
-//   // MatStepperModule,
-// } from '@angular/material';
+import { NgModule } from '@angular/core';
+
+import {
+  // MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  // MatChipsModule,
+  // MatDatepickerModule,
+  MatDialogModule,
+  // MatExpansionModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  // MatProgressSpinnerModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  // MatSortModule,
+  // MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  // MatStepperModule,
+} from '@angular/material';
 
 // import { MatNativeDateModule, MatRippleModule } from '@angular/material';
 // import { CdkTableModule } from '@angular/cdk/table';
