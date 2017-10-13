46bc84c7ff95cda2873b4de726ccee40ed26e017
James Billings <james_billings@me.com>
2017-10-13 16:42:37 -0600

    Remove unused translation features until needed again

--- src/client/app/application/footer/footer.component.ts
+++ src/client/app/application/footer/footer.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { UiConfig } from '../../shared/services/ui.config';
-import { Subscription } from 'rxjs/Subscription';
 /**
  * site footer component - renders the footer information
  */
@@ -12,23 +11,13 @@ import { Subscription } from 'rxjs/Subscription';
 })
 
 export class FooterComponent implements OnInit {
-  @Input() currentUser: any;
-  @Input() supportedLanguages: any;
-  @Output() onChangeLang = new EventEmitter();
-  public lang: any;
   public config: any;
-  public configSubscription: Subscription;
 
   constructor(public uiConfig: UiConfig) { }
 
   ngOnInit() {
-    this.lang = this.supportedLanguages[0].code;
     this.uiConfig.get('footer').take(1).subscribe((config) => {
       this.config = config.config;
     });
-  }
-
-  public selectLang(lang: any) {
-    this.onChangeLang.emit(lang.value);
   }
 }
