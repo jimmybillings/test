4c88ffcb03f89a83d5ed8a514765f01fb7cbfd2a
James Billings <james_billings@me.com>
2017-11-01 15:14:01 -0600

    CRUX-2782 fee config actions

---
+++ src/client/app/store/fee-config/fee-config.actions.spec.ts
@@ -0,0 +1,8 @@
+import { ActionFactory, InternalActionFactory } from './fee-config.actions';
+import { ActionsSpecHelper } from '../spec-helpers/actions.spec-helper';
+
+export function main() {
+  describe('Fee Config Actions', () => {
+    let actionsSpecHelper: ActionsSpecHelper = new ActionsSpecHelper();
+  });
+}
