8b5b1362005ee245b4a392d804da546bd4e405b7
James Billings <james_billings@me.com>
2017-11-09 10:07:54 -0700

    CRUX-2844 adds billing UI to quote send process

--- src/client/app/+commerce/+quote/+edit/components/tabs/quote-edit-send-tab.component.ts
+++ src/client/app/+commerce/+quote/+edit/components/tabs/quote-edit-send-tab.component.ts
@@ -1,6 +1,16 @@
+import { Common } from '../../../../../shared/utilities/common.functions';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { User, Account } from '../../../../../shared/interfaces/user.interface';
 import { Tab } from '../../../../components/tabs/tab';
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { QuoteOptions } from '../../../../../shared/interfaces/commerce.interface';
+import {
+  QuoteOptions,
+  QuoteRecipient,
+  QuoteRecipientBillingAccount,
+  QuoteRecipientInvoiceContact,
+  QuoteRecipientUser,
+} from '../../../../../shared/interfaces/commerce.interface';
 import { AppStore } from '../../../../../app.store';
 import { Pojo } from '../../../../../shared/interfaces/common.interface';
 
@@ -12,24 +22,47 @@ import { Pojo } from '../../../../../shared/interfaces/common.interface';
 })
 
 export class QuoteEditSendTabComponent extends Tab {
-  public config: Pojo;
-  constructor(
-    private store: AppStore
-  ) {
+
+  constructor(private store: AppStore) {
     super();
-    this.config = this.store.snapshotCloned(state => state.uiConfig.components.cart.config);
+    this.store.select(state => state.quoteEdit).subscribe(c => console.log(c));
   }
 
   public onSubmitSendQuote(options: QuoteOptions): void {
-    this.store.dispatch(factory => factory.quoteEdit.saveRecipientInformationOnQuote(
-      {
-        ownerEmail: options.ownerEmail,
-        expirationDate: new Date(options.expirationDate).toISOString(),
-        purchaseType: options.purchaseType.split(' ').join(''),
-        offlineAgreementId: options.offlineAgreementId
-      }
-    ));
+    this.store.dispatch(factory => factory.quoteEdit.saveRecipientInformationOnQuote({
+      ownerEmail: options.ownerEmail,
+      expirationDate: new Date(options.expirationDate).toISOString(),
+      purchaseType: options.purchaseType.split(' ').join(''),
+      offlineAgreementId: options.offlineAgreementId
+    }));
     this.goToNextTab();
+  }
+
+  public get user(): Observable<QuoteRecipientUser> {
+    return this.store.select(state => state.quoteEdit)
+      .map(quoteEdit => quoteEdit.recipient.user);
+  }
+
+  public get billingAccount(): Observable<QuoteRecipientBillingAccount> {
+    return this.store.select(state => state.quoteEdit)
+      .map(quoteEdit => quoteEdit.recipient.billingAccount);
+  }
+
+  public get invoiceContact(): Observable<QuoteRecipientInvoiceContact> {
+    return this.store.select(state => state.quoteEdit)
+      .map(quoteEdit => quoteEdit.recipient.invoiceContact);
+  }
+
+  public userSelect(user: User) {
+    this.store.dispatch(factory => factory.quoteEdit.addUserToQuote(user));
+  }
+
+  public accountSelect(account: Account) {
+    this.store.dispatch(factory => factory.quoteEdit.addBillingAccountToQuote(account));
+  }
+
+  public invoiceContactSelect(info: any) {
+    console.log(info);
   }
 
 }
