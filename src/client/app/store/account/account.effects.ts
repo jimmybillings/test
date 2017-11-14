30bccf2e63b4b1ce4c092849692a21514150ec96
James Billings <james_billings@me.com>
2017-11-13 23:13:36 -0700

    CRUX-2844 mvoe data mapping out of service and into effects

--- src/client/app/store/account/account.effects.ts
+++ src/client/app/store/account/account.effects.ts
@@ -1,8 +1,9 @@
+import { SendDetailsBillingAccount } from '../../shared/interfaces/commerce.interface';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Action } from '@ngrx/store';
 import { Effect, Actions } from '@ngrx/effects';
-
+import { Account } from '../../shared/interfaces/user.interface';
 import { AppStore } from '../../app.store';
 import { AccountService } from './account.service';
 import * as AccountActions from './account.actions';
@@ -12,12 +13,33 @@ export class AccountEffects {
 
   @Effect()
   public getAccountForQuoteAdmin: Observable<Action> = this.actions
-    .ofType(AccountActions.GetAccountForQuoteAdmin.Type)
-    .switchMap((action: AccountActions.GetAccountForQuoteAdmin) =>
-      this.service.getAccount(action.accountId)
-        .map(response =>
-          this.store.create(factory => factory.account.getAccountForQuoteAdminSuccess(response))
-        )
+    .ofType(AccountActions.GetAccountForQuoteAdmin.Type, AccountActions.GetAccountForQuoteAdminOnUserAdd.Type)
+    .switchMap((action: AccountActions.GetAccountForQuoteAdmin | AccountActions.GetAccountForQuoteAdminOnUserAdd) =>
+      this.service.getAccount(action.accountId, 'onBeforeRequest')
+        .map((account: Account) => (
+          {
+            id: account.id,
+            name: account.name,
+            creditExemption: account.creditExemption || null,
+            licensingVertical: account.licensingVertical || null,
+            paymentTermsDays: account.paymentTermsDays || null,
+            purchaseOnCredit: account.purchaseOnCredit || null,
+            invoiceContactId: account.invoiceContactId,
+            salesOwner: account.salesOwner || null
+          }
+        ))
+        .map((billingAccount: SendDetailsBillingAccount) => {
+          return (action.onUserAdd) ?
+            this.store.create(factory => factory.account.getAccountForQuoteAdminOnUserAddSuccess(billingAccount)) :
+            this.store.create(factory => factory.account.getAccountForQuoteAdminSuccess(billingAccount))
+        })
+    );
+
+  @Effect()
+  public getAccountForQuoteAdminSuccess: Observable<Action> = this.actions
+    .ofType(AccountActions.GetAccountForQuoteAdminSuccess.Type, AccountActions.GetAccountForQuoteAdminOnUserAddSuccess.Type)
+    .map((action: AccountActions.GetAccountForQuoteAdminSuccess | AccountActions.GetAccountForQuoteAdminOnUserAddSuccess) =>
+      this.store.create(factory => factory.user.getAllUsersByAccountId(action.account.id))
     );
 
   constructor(private actions: Actions, private store: AppStore, private service: AccountService) { }
--- src/client/app/store/user/user.service.ts
+++ src/client/app/store/user/user.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { FutureApiService } from '../api/api.service';
-import { Api } from '../../shared/interfaces/api.interface';
+import { Api, LoadingIndicatorOption } from '../../shared/interfaces/api.interface';
 import { User } from '../../shared/interfaces/user.interface';
 
 @Injectable()
 export class FutureUserService {
   constructor(private apiService: FutureApiService) { }
 
-  public getUsersByAccountId(accountId: number): Observable<User[]> {
+  public getUsersByAccountId(accountId: number, loadingIndicator: LoadingIndicatorOption): Observable<User[]> {
     return this.apiService.get(Api.Identities, 'user/searchFields',
-      { parameters: { 'fields': 'accountId', 'values': `${accountId}`, 'n': '500' } })
+      {
+        loadingIndicator: loadingIndicator,
+        parameters: { 'fields': 'accountId', 'values': `${accountId}`, 'n': '500' }
+      })
       .map(users => users.items);
   }
 
