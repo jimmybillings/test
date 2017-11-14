935f8680f9211531673ff537d30ad5d7fdda6a75
jhyde <jeff@jeffhyde.com>
2017-11-14 08:36:27 -0700

    CRUX-2852 initial start on confirmation display

--- src/client/app/+commerce/+quote/+edit/components/tabs/quote-edit-confirm-tab.component.ts
+++ src/client/app/+commerce/+quote/+edit/components/tabs/quote-edit-confirm-tab.component.ts
@@ -16,26 +16,7 @@ import { AppStore } from '../../../../../app.store';
   moduleId: module.id,
   selector: 'quote-edit-confirm-tab-component',
   templateUrl: 'quote-edit-confirm-tab.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  styles: [`
-    .recipient {
-      background-color: rgba(255,255,255,0.65);
-      color: rgba(0,0,0,0.7);
-      max-width: calc(33.333% - 12px);
-      min-width: calc(33.333% - 12px);
-      padding: 15px;
-      margin-bottom: 15px;
-    }
-    .recipient span {
-      display: inline-block;
-      font-weight:bold;
-    }
-    .recipient ul {
-      list-style-type: none;
-      padding-left: 0;
-    }
-  `]
-
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class QuoteEditConfirmTabComponent {
