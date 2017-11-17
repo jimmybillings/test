5dbc878fb03a0a347160dfed54b72c23be6fade8
James Billings <james_billings@me.com>
2017-11-16 23:49:35 -0700

    CRUX-2844 spec for picklist component

--- src/client/app/+commerce/+quote/+edit/components/wz-form-picklist.component.ts
+++ src/client/app/+commerce/+quote/+edit/components/wz-form-picklist.component.ts
@@ -1,12 +1,10 @@
-import { BehaviorSubject } from 'rxjs/Rx';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Pojo } from '../../../../shared/interfaces/common.interface';
 import { Component, ChangeDetectionStrategy, ElementRef, Input, Output, EventEmitter } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { FormModel } from '../../../../shared/modules/wz-form/wz.form.model';
 import { WzFormBase } from '../../../../shared/modules/wz-form/wz.form-base';
-/**
- * Home page component - renders the home page
- */
+
 @Component({
   moduleId: module.id,
   selector: 'wz-form-picklist-component',
@@ -15,10 +13,10 @@ import { WzFormBase } from '../../../../shared/modules/wz-form/wz.form-base';
 })
 
 export class WzFormPicklistComponent extends WzFormBase {
-  public labels: BehaviorSubject<Pojo[]> = new BehaviorSubject([]);
+  public labels: BehaviorSubject<Pojo[]>;
   @Input() title: string;
   @Input()
-  set displayProperties(properties: string[]) {
+  set displayProperties(properties: Pojo) {
     const tempLabels: Pojo[] = Object.keys(properties || [])
       .filter(property => property !== 'field' && property !== 'id' && property !== 'name')
       .map((property: string) => {
@@ -26,7 +24,7 @@ export class WzFormPicklistComponent extends WzFormBase {
         label = `QUOTE.EDIT.${label.toUpperCase()}_KEY`;
         return { label: label, value: properties[property as any] };
       });
-    this.labels.next(tempLabels);
+    this.labels = new BehaviorSubject(tempLabels);
   }
   @Output() selectContact: EventEmitter<Pojo> = new EventEmitter();
 
