db4244c1fa8f84010e51eda7d3a9f35499ba8cbb
James Billings <james_billings@me.com>
2017-11-16 23:50:43 -0700

    CRUX-2844 spec for account actions

--- src/client/app/store/account/account.actions.spec.ts
+++ src/client/app/store/account/account.actions.spec.ts
@@ -4,5 +4,55 @@ import { ActionsSpecHelper } from '../spec-helpers/actions.spec-helper';
 export function main() {
   describe('Account Actions', () => {
     let actionsSpecHelper: ActionsSpecHelper = new ActionsSpecHelper();
+
+    actionsSpecHelper.generateTestFor({
+      factoryMethod: {
+        class: ActionFactory,
+        name: 'getAccountForQuoteAdmin',
+        parameters: [1]
+      },
+      expectedAction: {
+        type: '[Account] Get Account For Quote Admin',
+        accountId: 1,
+        onUserAdd: false
+      }
+    });
+
+    actionsSpecHelper.generateTestFor({
+      factoryMethod: {
+        class: InternalActionFactory,
+        name: 'getAccountForQuoteAdminSuccess',
+        parameters: [{ account: 'Some account' }]
+      },
+      expectedAction: {
+        type: '[Account] Get Account For Quote Admin Success',
+        account: { account: 'Some account' }
+      }
+    });
+
+    actionsSpecHelper.generateTestFor({
+      factoryMethod: {
+        class: InternalActionFactory,
+        name: 'getAccountForQuoteAdminOnUserAdd',
+        parameters: [{ accountId: 1 }]
+      },
+      expectedAction: {
+        type: '[Account] Get Account For Quote Admin On User Add',
+        accountId: { accountId: 1 },
+        onUserAdd: true
+      }
+    });
+
+    actionsSpecHelper.generateTestFor({
+      factoryMethod: {
+        class: InternalActionFactory,
+        name: 'getAccountForQuoteAdminOnUserAddSuccess',
+        parameters: [{ account: 'Some account' }]
+      },
+      expectedAction: {
+        type: '[Account] Get Account For Quote Admin On User Add Success',
+        account: { account: 'Some account' }
+      }
+    });
   });
 }
