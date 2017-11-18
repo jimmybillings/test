1353ac35d066a66eb5448f510f51dec205bd40b0
James Billings <james_billings@me.com>
2017-11-18 08:47:00 -0700

    INTERNAL - update config to use the latest rxjs, needed for updating to Angular 5

--- tools/tasks/seed/build.bundle.rxjs.ts
+++ tools/tasks/seed/build.bundle.rxjs.ts
@@ -18,15 +18,16 @@ export = (done: any) => {
     paths: {
       'n:*': 'node_modules/*',
       'rxjs/*': 'node_modules/rxjs/*.js',
+      'rxjs/operators': 'node_modules/rxjs/operators/index.js'
     },
     map: {
       'rxjs': 'n:rxjs',
     },
     packages: {
-      'rxjs': {main: 'Rx.js', defaultExtension: 'js'},
+      'rxjs': { main: 'Rx.js', defaultExtension: 'js' },
     }
   });
   builder.bundle('rxjs', 'node_modules/.tmp/Rx.min.js', options)
     .then(() => done())
-    .catch((error:any) => done(error));
+    .catch((error: any) => done(error));
 };
