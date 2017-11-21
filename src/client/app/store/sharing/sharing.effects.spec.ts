28ab110681b91e2f87f1bf27ab02527027459013
James Billings <james_billings@me.com>
2017-11-21 09:56:35 -0700

    CRUX-2285 test sharing effects

--- src/client/app/store/sharing/sharing.effects.spec.ts
+++ src/client/app/store/sharing/sharing.effects.spec.ts
@@ -11,5 +11,71 @@ export function main() {
         effectsSpecHelper.mockNgrxEffectsActions, effectsSpecHelper.mockStore, effectsSpecHelper.mockService
       );
     }
+
+    effectsSpecHelper.generateTestsFor({
+      effectName: 'createAssetShareLink',
+      effectsInstantiator: instantiator,
+      inputAction: {
+        type: SharingActions.CreateAssetShareLink.Type,
+        assetId: 'someAssetId',
+        markers: { some: 'markers' }
+      },
+      serviceMethod: {
+        name: 'createAssetShareLink',
+        expectedArguments: ['someAssetId', { some: 'markers' }],
+        returnsObservableOf: 'link'
+      },
+      outputActionFactories: {
+        success: {
+          sectionName: 'sharing',
+          methodName: 'createAssetShareLinkSuccess',
+          expectedArguments: ['link']
+        }
+      }
+    });
+
+    effectsSpecHelper.generateTestsFor({
+      effectName: 'emailAssetShareLink',
+      effectsInstantiator: instantiator,
+      inputAction: {
+        type: SharingActions.EmailAssetShareLink.Type,
+        assetId: 'someAssetId',
+        markers: { some: 'markers' },
+        parameters: { some: 'paramaters' }
+      },
+      serviceMethod: {
+        name: 'emailAssetShareLink',
+        expectedArguments: ['someAssetId', { some: 'markers' }, { some: 'paramaters' }],
+      },
+      outputActionFactories: {
+        success: {
+          sectionName: 'snackbar',
+          methodName: 'display',
+          expectedArguments: ['ASSET.SHARING.SHARED_CONFIRMED_MESSAGE']
+        }
+      }
+    });
+
+    effectsSpecHelper.generateTestsFor({
+      effectName: 'emailCollectionShareLink',
+      effectsInstantiator: instantiator,
+      inputAction: {
+        type: SharingActions.EmailCollectionShareLink.Type,
+        collectionId: 'someCollectionId',
+        parameters: { some: 'paramaters' }
+      },
+      serviceMethod: {
+        name: 'emailCollectionShareLink',
+        expectedArguments: ['someCollectionId', { some: 'paramaters' }],
+      },
+      outputActionFactories: {
+        success: {
+          sectionName: 'snackbar',
+          methodName: 'display',
+          expectedArguments: ['ASSET.SHARING.SHARED_CONFIRMED_MESSAGE']
+        }
+      }
+    });
+
   });
 }
